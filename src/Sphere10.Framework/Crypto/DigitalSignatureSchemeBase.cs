a3ab6b4b073e53346cacc48fc1b34d0264aa9832
Herman Schoenfeld <herman@sphere10.com>
2021-02-13 22:37:26 +1000

    Framework: support for stateless digital signature schemes

--- src/Sphere10.Framework/Crypto/DigitalSignatureSchemeBase.cs
+++ src/Sphere10.Framework/Crypto/DigitalSignatureSchemeBase.cs
@@ -3,85 +3,86 @@ using System.Runtime.CompilerServices;
 
 namespace Sphere10.Framework {
 
-	public abstract class DigitalSignatureSchemeBase<TPrivateKey, TPublicKey> : IDigitalSignatureScheme 
-		where TPrivateKey : IPrivateKey 
-		where TPublicKey : IPublicKey {
-		private readonly CHF _mdCHF;
-		private readonly int _saltSizeBytes;
 
-		protected DigitalSignatureSchemeBase(CHF messageDigestCHF, int saltSizeBytes = 0) {
-			_mdCHF = messageDigestCHF;
-			_saltSizeBytes = saltSizeBytes;
-			Traits = saltSizeBytes > 0 ? DigitalSignatureSchemeTraits.SaltedSignatures : 0;
-		}
+    public abstract class DigitalSignatureSchemeBase<TPrivateKey, TPublicKey> : IDigitalSignatureScheme
+    where TPrivateKey : IPrivateKey
+    where TPublicKey : IPublicKey {
+    private readonly CHF _mdCHF;
+    private readonly int _saltSizeBytes;
 
-		public DigitalSignatureSchemeTraits Traits { get; }
+    protected DigitalSignatureSchemeBase(CHF messageDigestCHF, int saltSizeBytes = 0) {
+        _mdCHF = messageDigestCHF;
+        _saltSizeBytes = saltSizeBytes;
+        Traits = saltSizeBytes > 0 ? DigitalSignatureSchemeTraits.SaltedSignatures : 0;
+    }
 
-		public abstract bool TryParsePublicKey(ReadOnlySpan<byte> bytes, out TPublicKey publicKey);
-		
-		public abstract bool TryParsePrivateKey(ReadOnlySpan<byte> bytes, out TPrivateKey privateKey);
-		
-		public TPrivateKey GeneratePrivateKey()
-			=> (TPrivateKey)((IDigitalSignatureScheme)this).GeneratePrivateKey();
-	
-		public abstract TPrivateKey GeneratePrivateKey(ReadOnlySpan<byte> seed);
-		
-		public abstract TPublicKey DerivePublicKey(TPrivateKey privateKey, ulong signerNonce);
-		
-		public abstract bool IsPublicKey(TPrivateKey privateKey, ReadOnlySpan<byte> publicKeyBytes);
+    public DigitalSignatureSchemeTraits Traits { get; }
 
-		public virtual byte[] GenerateSalt(ReadOnlySpan<byte> message) {
-			return Tools.Crypto.GenerateCryptographicallyRandomBytes(_saltSizeBytes);
-		}
+    public abstract bool TryParsePublicKey(ReadOnlySpan<byte> bytes, out TPublicKey publicKey);
 
-		public virtual ReadOnlySpan<byte> ExtractSaltFromSignature(ReadOnlySpan<byte> signature) {
-			return signature.Slice(^_saltSizeBytes);
-		}
+    public abstract bool TryParsePrivateKey(ReadOnlySpan<byte> bytes, out TPrivateKey privateKey);
 
-		public virtual byte[] CalculateMessageDigest(ReadOnlySpan<byte> message, ReadOnlySpan<byte> salt)
-			=> Traits == DigitalSignatureSchemeTraits.SaltedSignatures ? SMAC(message, salt) : Hashers.Hash(_mdCHF, message);
+    public TPrivateKey GeneratePrivateKey()
+        => (TPrivateKey)((IDigitalSignatureScheme)this).GeneratePrivateKey();
 
-		public abstract byte[] SignDigest(TPrivateKey privateKey, ReadOnlySpan<byte> messageDigest, ReadOnlySpan<byte> salt, ulong signerNonce);
+    public abstract TPrivateKey GeneratePrivateKey(ReadOnlySpan<byte> seed);
 
-		public abstract bool VerifyDigest(ReadOnlySpan<byte> signature, ReadOnlySpan<byte> messageDigest, ReadOnlySpan<byte> publicKey);
+    public abstract TPublicKey DerivePublicKey(TPrivateKey privateKey, ulong signerNonce);
 
-		bool IDigitalSignatureScheme.TryParsePublicKey(ReadOnlySpan<byte> bytes, out IPublicKey publicKey) {
-			var result = TryParsePublicKey(bytes, out var pub);
-			publicKey = pub;
-			return result;
-		}
+    public abstract bool IsPublicKey(TPrivateKey privateKey, ReadOnlySpan<byte> publicKeyBytes);
 
-		bool IDigitalSignatureScheme.TryParsePrivateKey(ReadOnlySpan<byte> bytes, out IPrivateKey privateKey) {
-			var result = TryParsePrivateKey(bytes, out var priv);
-			privateKey = priv;
-			return result;
-		}
+    public virtual byte[] GenerateSalt(ReadOnlySpan<byte> message) {
+        return Tools.Crypto.GenerateCryptographicallyRandomBytes(_saltSizeBytes);
+    }
 
-		IPrivateKey IDigitalSignatureScheme.CreatePrivateKey(ReadOnlySpan<byte> seed)
-			=> GeneratePrivateKey(seed);
+    public virtual ReadOnlySpan<byte> ExtractSaltFromSignature(ReadOnlySpan<byte> signature) {
+        return signature.Slice(^_saltSizeBytes);
+    }
 
-		IPublicKey IDigitalSignatureScheme.DerivePublicKey(IPrivateKey privateKey, ulong signerNonce)
-			=> DerivePublicKey((TPrivateKey)privateKey, signerNonce);
+    public virtual byte[] CalculateMessageDigest(ReadOnlySpan<byte> message, ReadOnlySpan<byte> salt)
+        => Traits == DigitalSignatureSchemeTraits.SaltedSignatures ? SMAC(message, salt) : Hashers.Hash(_mdCHF, message);
 
-		bool IDigitalSignatureScheme.IsPublicKey(IPrivateKey privateKey, ReadOnlySpan<byte> publicKeyBytes)
-			=> IsPublicKey((TPrivateKey)privateKey, publicKeyBytes);
+    public abstract byte[] SignDigest(TPrivateKey privateKey, ReadOnlySpan<byte> messageDigest, ReadOnlySpan<byte> salt, ulong signerNonce);
 
-		byte[] IDigitalSignatureScheme.SignDigest(IPrivateKey privateKey, ReadOnlySpan<byte> messageDigest, ReadOnlySpan<byte> salt, ulong signerNonce)
-			=> SignDigest((TPrivateKey)privateKey, messageDigest, salt, signerNonce);
+    public abstract bool VerifyDigest(ReadOnlySpan<byte> signature, ReadOnlySpan<byte> messageDigest, ReadOnlySpan<byte> publicKey);
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private byte[] SMAC(ReadOnlySpan<byte> message, ReadOnlySpan<byte> seed) {
-			using (Hashers.BorrowHasher(_mdCHF, out var hasher)) {
-				var digest = hasher.Compute(message);
-				hasher.Transform(seed);
-				hasher.Transform(digest);
-				var innerHash = hasher.GetResult();
-				hasher.Transform(seed);
-				hasher.Transform(innerHash);
-				return hasher.GetResult();
-			}
-		}
+    bool IDigitalSignatureScheme.TryParsePublicKey(ReadOnlySpan<byte> bytes, out IPublicKey publicKey) {
+        var result = TryParsePublicKey(bytes, out var pub);
+        publicKey = pub;
+        return result;
+    }
 
-	}
+    bool IDigitalSignatureScheme.TryParsePrivateKey(ReadOnlySpan<byte> bytes, out IPrivateKey privateKey) {
+        var result = TryParsePrivateKey(bytes, out var priv);
+        privateKey = priv;
+        return result;
+    }
+
+    IPrivateKey IDigitalSignatureScheme.CreatePrivateKey(ReadOnlySpan<byte> seed)
+        => GeneratePrivateKey(seed);
+
+    IPublicKey IDigitalSignatureScheme.DerivePublicKey(IPrivateKey privateKey, ulong signerNonce)
+        => DerivePublicKey((TPrivateKey)privateKey, signerNonce);
+
+    bool IDigitalSignatureScheme.IsPublicKey(IPrivateKey privateKey, ReadOnlySpan<byte> publicKeyBytes)
+        => IsPublicKey((TPrivateKey)privateKey, publicKeyBytes);
+
+    byte[] IDigitalSignatureScheme.SignDigest(IPrivateKey privateKey, ReadOnlySpan<byte> messageDigest, ReadOnlySpan<byte> salt, ulong signerNonce)
+        => SignDigest((TPrivateKey)privateKey, messageDigest, salt, signerNonce);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private byte[] SMAC(ReadOnlySpan<byte> message, ReadOnlySpan<byte> seed) {
+        using (Hashers.BorrowHasher(_mdCHF, out var hasher)) {
+            var digest = hasher.Compute(message);
+            hasher.Transform(seed);
+            hasher.Transform(digest);
+            var innerHash = hasher.GetResult();
+            hasher.Transform(seed);
+            hasher.Transform(innerHash);
+            return hasher.GetResult();
+        }
+    }
+
+}
 
 }
---
+++ src/Sphere10.Framework/Crypto/StatelessDigitalSignatureScheme.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sphere10.Framework {
+
+    /// <summary>
+    /// Represents a stateless digital signature scheme. One where knowledge of nonces from prior signatures are not ever needed. These
+    /// are mainly ECDSA, Schnorr and SPHINCS+.
+    /// </summary>
+    /// <typeparam name="TPrivateKey"></typeparam>
+    /// <typeparam name="TPublicKey"></typeparam>
+    public abstract class StatelessDigitalSignatureScheme<TPrivateKey, TPublicKey> : DigitalSignatureSchemeBase<TPrivateKey, TPublicKey>
+        where TPrivateKey : IPrivateKey
+        where TPublicKey : IPublicKey {
+
+        protected const int DefaultNonce = 0;
+
+        public StatelessDigitalSignatureScheme(CHF messageDigestCHF, int saltSizeBytes = 0) 
+            : base(messageDigestCHF, saltSizeBytes) {
+        }
+
+        public sealed override TPublicKey DerivePublicKey(TPrivateKey privateKey, ulong signerNonce)
+            => DerivePublicKey(privateKey);
+
+        public abstract TPublicKey DerivePublicKey(TPrivateKey privateKey);
+
+        public sealed override byte[] SignDigest(TPrivateKey privateKey, ReadOnlySpan<byte> messageDigest, ReadOnlySpan<byte> salt, ulong signerNonce)
+            => SignDigest(privateKey, messageDigest, salt);
+
+        public abstract byte[] SignDigest(TPrivateKey privateKey, ReadOnlySpan<byte> messageDigest, ReadOnlySpan<byte> salt);
+        
+    }
+
+}

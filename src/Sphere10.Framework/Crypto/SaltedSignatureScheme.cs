ea70438841ac395911b5a2cf52e705a2243d2ad8
Herman Schoenfeld <herman@sphere10.com>
2021-02-14 09:17:05 +1000

    Framework: cleanup SaltedSignatureScheme decorator

--- src/Sphere10.Framework/Crypto/SaltedSignatureScheme.cs
+++ src/Sphere10.Framework/Crypto/SaltedSignatureScheme.cs
@@ -30,32 +30,44 @@ namespace Sphere10.Framework {
         public override byte[] CalculateMessageDigest(ReadOnlySpan<byte> message) {
             var salt = GenerateSalt(message);
             var messageDigest = SMAC(message, salt);
-            // Note: The salt aggregated to end of message digest here, but the salt is
-            // extracted duing internal signing and verifying!
+            // Note: this returns a salted SMAC. The salt is appended to the end of the SMAC
+            // so signature/verification can extract/use it.
             return JoinSalt(messageDigest, salt);
         }
 
-        public override byte[] SignDigest(IPrivateKey privateKey, ReadOnlySpan<byte> messageDigest, ulong signerNonce) {
+        public override byte[] SignDigest(IPrivateKey privateKey, ReadOnlySpan<byte> saltedSMAC, ulong signerNonce) {
             Guard.ArgumentNotNull(privateKey, nameof(privateKey));
-            SplitSalt(messageDigest, out var actualMessageDigest, out var salt);
-            var signature = Internal.SignDigest(privateKey, actualMessageDigest, signerNonce);
+            SplitSalt(saltedSMAC, out var smac, out var salt);
+            var signature = Internal.SignDigest(privateKey, smac, signerNonce);
             var saltedSignature = JoinSalt(signature, salt);
             return saltedSignature;
         }
 
-        public override bool VerifyDigest(ReadOnlySpan<byte> saltedSignature, ReadOnlySpan<byte> messageDigest, ReadOnlySpan<byte> publicKey) {
-            SplitSalt(messageDigest, out var actualDigest, out var saltInDigest);
+        public override bool VerifyDigest(ReadOnlySpan<byte> saltedSignature, ReadOnlySpan<byte> saltedSMAC, ReadOnlySpan<byte> publicKey) {
+            SplitSalt(saltedSMAC, out var messageDigest, out var saltInSMAC);
             SplitSalt(saltedSignature, out var signature, out var saltInSignature);
-            if (!saltInDigest.SequenceEqual(saltInSignature))
+            if (!saltInSMAC.SequenceEqual(saltInSignature))
                 return false;
-            return Internal.VerifyDigest(signature, actualDigest, publicKey);
+            return Internal.VerifyDigest(signature, messageDigest, publicKey);
         }
 
 
+        /// <summary>
+        /// Returns an SMAC of the argument.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="seed"></param>
+        /// <returns>H(seed || H(seed || H(message)))</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private byte[] SMAC(ReadOnlySpan<byte> message, ReadOnlySpan<byte> seed)
             => HMAC(Internal.CalculateMessageDigest(message), seed);
 
+        /// <summary>
+        /// Returns an HMAC of the argument.
+        /// </summary>
+        /// <param name="digest"></param>
+        /// <param name="seed"></param>
+        /// <returns>H(seed || H(seed || digest))</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private byte[] HMAC(ReadOnlySpan<byte> digest, ReadOnlySpan<byte> seed) {
             using (Hashers.BorrowHasher(SaltingCHF, out var hasher)) {

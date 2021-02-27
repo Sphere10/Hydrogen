f5cafdbf2dc93a12d53943b0da329c5c905b2023
Herman Schoenfeld <herman@sphere10.com>
2021-02-28 07:45:51 +1000

    Helium: initial commit

---
+++ src/Sphere10.Helium.Loader/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Sphere10.Helium.Loader {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

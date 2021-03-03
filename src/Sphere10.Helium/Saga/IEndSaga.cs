12d7f045c19f8a3dcde964b81a2d053de4f22a09
cobanicuss <79815312+cobanicuss@users.noreply.github.com>
2021-03-03 21:19:41 +1000

    Add boxing of the IMessage Interface to IEndSaga and IStartSaga.

--- src/Sphere10.Helium/Saga/IEndSaga.cs
+++ src/Sphere10.Helium/Saga/IEndSaga.cs
@@ -1,8 +1,9 @@
 ﻿using Sphere10.Helium.Handler;
+using Sphere10.Helium.MessageType;
 
 namespace Sphere10.Helium.Saga
 {
-    public interface IEndSaga<T> : IHandleMessage<T>
+    public interface IEndSaga<T> : IHandleMessage<T> where T : IMessage
     {
     }
 }
--- src/Sphere10.Helium/Saga/IStartSaga.cs
+++ src/Sphere10.Helium/Saga/IStartSaga.cs
@@ -1,8 +1,9 @@
 ﻿using Sphere10.Helium.Handler;
+using Sphere10.Helium.MessageType;
 
 namespace Sphere10.Helium.Saga
 {
-    public interface IStartSaga<T> : IHandleMessage<T>
+    public interface IStartSaga<T> : IHandleMessage<T> where T: IMessage
     {
     }
 }

b99dd9fdedba0c3780a90a5ef3d71d16491c5a35
hamish-rose <hamishr@protonmail.com>
2021-03-09 14:11:26 +1300

    Framework: add BoundedStream unit tests

---
+++ src/Sphere10.Framework.Tests/Streams/BoundedStreamTests.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Sphere10.Framework.Tests
+{
+
+    public class BoundedStreamTests
+    {
+        [Test]
+        public void BoundedStreamAbsoluteSeek()
+        {
+            var inner = new MemoryStream();
+            var bounded = new BoundedStream(inner, 100, 199);
+
+            Assert.Throws<StreamOutOfBoundsException>(() => bounded.Seek(0, SeekOrigin.Begin));
+            bounded.Seek(bounded.MinAbsolutePosition, SeekOrigin.Begin);
+        }
+
+        [Test]
+        public void BoundedStreamRelativeSeek()
+        {
+            var inner = new MemoryStream();
+            var bounded = new BoundedStream(inner, 100, 199)
+            {
+                UseRelativeOffset = true
+            };
+
+            Assert.Throws<StreamOutOfBoundsException>(() => bounded.Seek(bounded.MinAbsolutePosition, SeekOrigin.Begin));
+            bounded.Seek(0, SeekOrigin.Begin);
+        }
+    }
+
+}

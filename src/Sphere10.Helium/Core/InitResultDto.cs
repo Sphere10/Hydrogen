e5ead52767030ef532b4c249a4c773cac17da15d
cobanicuss <79815312+cobanicuss@users.noreply.github.com>
2021-03-18 18:22:57 +1000

    Adding Helium Core to dealy with Helium Initialization.

---
+++ src/Sphere10.Helium/Core/InitResultDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Sphere10.Helium.Core
+{
+    public record InitResultDto
+    {
+        public InitResultDto(bool isInitSuccessful, string errorList)
+        {
+            IsInitSuccessful = isInitSuccessful;
+            ErrorList = errorList;
+        }
+
+        public bool IsInitSuccessful { get; init; }
+
+        public string ErrorList { get; init; }
+    }
+}
---
+++ src/Sphere10.Helium/Core/RegisterCore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Sphere10.Helium.Bus;
+using Sphere10.Helium.Endpoint;
+using Sphere10.Helium.Handler;
+using Sphere10.Helium.Message;
+using Sphere10.Helium.Saga;
+
+namespace Sphere10.Helium.Core
+{
+    public class RegisterCore : IRegisterCoreTypes
+    {
+        public IList<IHandleMessage<IMessage>> HandlerList { get; set; }
+
+        public IList<IHandleTimeout<IMessage>> TimeoutList { get; set; }
+
+        public IList<IStartSaga<IMessage>> StartSagaList { get; set; }
+
+        public IList<IEndSaga<IMessage>> EndSagaList { get; set; }
+
+        public IBus Bus { get; set; }
+
+        public IEndpointConfiguration EndpointConfiguration { get; set; }
+
+        public InitResultDto InitializeHelium()
+        {
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}

17a3b7887afff6cbc731e6be2e881d24e6a0b7e2
Herman Schoenfeld <herman@sphere10.com>
2021-05-03 10:37:35 +1000

    SP10 Framework: Pipe and Protocol abstractions drafts

---
+++ src/Sphere10.Framework/Environment/ParentChildProcessPipe.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Sphere10.Framework.Environment {
+//	public class ParentChildProcessPipe {
+
+
+//		public void RunChild(IProtocol) 
+
+
+//	}
+//}
---
+++ src/Sphere10.Framework/Protocol/INetworkProtocol.cs
@@ -0,0 +1,97 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Sphere10.Framework.Protocol {
+
+//	public interface IProtocolChannel<TEndpoint, TMessageID> {
+//		void Initiate(IHandshake handshake);
+
+//		void SendCommand(IProtocolCommand command);
+
+//		void SendRequest(IProtocolRequest request);
+
+//		Func<ResponseResult>? ReceiveCommand(IProtocolCommand command);
+
+//		Func<ResponseResult>? ReceiveHandshake(IHandshake handshake);
+
+//		Func<ResponseResult>? ReceiveResponse(IProtocolRequest request, IProtocolResponse response);
+
+//	}
+
+//	public class ResponseResult {
+
+//	}
+
+//	public interface IProtocolMessage<TEndpoint, TMessageID> {
+
+//		TMessageID ID { get; }
+
+//		TEndpoint Sender { get; }
+
+//		UInt16 MessageType { get; }
+
+//		UInt32 Nonce { get; }
+
+//		byte[] Payload { get; }
+//	}
+
+//	public interface IProtocolCommand<TEndpoint, TMessageID> : IProtocolMessage<TEndpoint, TMessageID> {
+
+//	}
+
+//	public interface IProtocolHandshake<TEndpoint, TMessageID> : IProtocolCommand<TEndpoint, TMessageID> {
+//	}
+
+	
+//	public interface IProtocolRequest<TEndpoint, TMessageID> : IProtocolMessage<TEndpoint, TMessageID> {
+
+//	}
+
+//	public interface IProtocolResponse<TEndpoint, TMessageID> : IProtocolMessage<TEndpoint, TMessageID> {
+
+//		TMessageID RequestID { get; }
+//	}
+
+//	public class ProtocolSpecification {
+
+//		RulesSpecification Rules { get; }
+//		public class RulesSpecification {
+
+//		}
+//	}
+
+
+//	[Flags]
+//	public enum ProtocolOptions {
+//		None	     = 0,
+//		UseDDoSNonce = 1 << 0, 
+
+//	}
+
+//	public class Foo {
+
+//		public void Bar() {
+
+
+//			var protocolSpecification = new ProtocolSpecification {
+
+//				Options = ProtocolOptions.UseDDoSNonce,
+
+//				Handshake = new Handshake1();
+
+//				Commands = new[] {
+//					new ProtocolCommand1(),
+//					new ProtocolCommand2(),
+//				},
+
+//				RequestResponses = new [] {
+//					new ProtocolRequestResponse(new Request1(), new Response1()),
+//					new ProtocolRequestResponse(new Request2(), new Response2()),
+//				},
+
+//			}
+//		}
+//	}
+
+//}

a2387684696b8bdfba14a14ac5b52f42f71c82e3
Herman Schoenfeld <herman@sphere10.com>
2021-05-07 22:51:27 +1000

    RandomHash2: cleanup and styling refactor

---
+++ src/Sphere10.Hydrogen.Core/Maths/RandomHash2Exception.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sphere10.Hydrogen.Core.Maths {
+	public class RandomHash2Exception : Exception {
+		public RandomHash2Exception(string message) : base(message) {
+		}
+	}
+}

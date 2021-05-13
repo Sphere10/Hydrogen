82faa40ccc5b4b713664977f395f0538da350258
Herman Schoenfeld <herman@sphere10.com>
2021-05-13 15:47:10 +1000

    Absorb Framework-v3 updates

---
+++ src/Sphere10.Framework/Environment/CommandLineArgSubCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Sphere10.Framework {
+	public class CommandLineArgSubCommand {
+		public CommandLineArgSubCommand(string name, CommandLineArg[] arguments) {
+			Name = name;
+			Arguments = arguments;
+		}
+
+		public string Name { get; }
+
+		public CommandLineArg[] Arguments { get; }
+	}
+}

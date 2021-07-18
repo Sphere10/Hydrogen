f54dacfe166c5bb0be958d6aa71cedf68edaccf2
hamish-rose <hamishr@protonmail.com>
2021-07-18 18:24:52 +1200

    Update VarIntTests.cs
    more test cases

--- tests/Sphere10.Framework.Tests/Values/VarIntTests.cs
+++ tests/Sphere10.Framework.Tests/Values/VarIntTests.cs
@@ -7,16 +7,20 @@ namespace Sphere10.Framework.Tests.Values
     public class VarIntTests
     {
         [Test]
+        [TestCase(ulong.MinValue, 1)]
         [TestCase((ulong)0xFC, 1)]
+        [TestCase((ulong)0xFD, 3)]
         [TestCase((ulong)0xFFFF, 3)]
+        [TestCase((ulong)0xFFFFF, 5)]
         [TestCase((ulong)0xFFFFFFFF, 5)]
-        [TestCase((ulong)0xFFFFFFFFFFFFFFFF, 9)]
+        [TestCase((ulong)0xFFFFFFFFF, 9)]
+        [TestCase(ulong.MaxValue, 9)]
         public void WriteAndRead(ulong value, int expectedByteLength)
         {
             var stream = new MemoryStream();
             VarInt a = new VarInt(value);
             stream.Write(a.ToBytes());
-            Assert.AreEqual(stream.Length, expectedByteLength);
+            Assert.AreEqual(expectedByteLength, stream.Length);
 
             stream.Seek(0, SeekOrigin.Begin);
             ulong b = VarInt.FromStream(stream);

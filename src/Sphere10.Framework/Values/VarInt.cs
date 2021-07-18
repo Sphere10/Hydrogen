b5143af5486a9632e850d1a0b5a520f7b586e4ab
hamish-rose <hamishr@protonmail.com>
2021-07-18 18:19:02 +1200

    SP10 Framework: VarInt
    varint implementation based on bitcoin, only supports unsigned integers

---
+++ src/Sphere10.Framework/Values/VarInt.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Sphere10.Framework {
+	public class VarInt {
+		
+		public ulong Value { get; }
+		
+		private static readonly EndianBitConverter BitConverter = EndianBitConverter.Little;
+		
+		public VarInt(ulong value) {
+			Value = value;
+		}
+
+		public static VarInt FromStream(Stream stream) {
+			var reader = new EndianBinaryReader(BitConverter, stream);
+			var prefix = stream.ReadByte();
+
+			if (prefix < 0xFD)
+				return new VarInt((byte)prefix);
+
+			if (prefix == 0xFD) {
+				return new VarInt(reader.ReadUInt16());
+			}
+
+			if (prefix == 0xFE)
+				return new VarInt(reader.ReadUInt32());
+
+			return new VarInt(reader.ReadUInt64());
+		}
+
+		public byte[] ToBytes() {
+
+			var builder = new ByteArrayBuilder();
+
+			if (Value < 0xFD)
+				builder.Append((byte)Value);
+			else if (Value <= 0xFFFF) {
+				builder.Append(0xFD);
+				builder.Append(BitConverter.GetBytes((ushort)Value));
+			} 
+			else if (Value <= 0xFFFFFFFF) {
+				builder.Append(0xFE);
+				builder.Append(BitConverter.GetBytes((uint)Value));
+			} else {
+				builder.Append(0xFF);
+				builder.Append(BitConverter.GetBytes(Value));
+			}
+
+			return builder.ToArray();
+		}
+
+		public static implicit operator ulong(VarInt v) => v.Value;
+	}
+}
---
+++ tests/Sphere10.Framework.Tests/Values/VarIntTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Sphere10.Framework.Tests.Values
+{
+    public class VarIntTests
+    {
+        [Test]
+        [TestCase((ulong)0xFC, 1)]
+        [TestCase((ulong)0xFFFF, 3)]
+        [TestCase((ulong)0xFFFFFFFF, 5)]
+        [TestCase((ulong)0xFFFFFFFFFFFFFFFF, 9)]
+        public void WriteAndRead(ulong value, int expectedByteLength)
+        {
+            var stream = new MemoryStream();
+            VarInt a = new VarInt(value);
+            stream.Write(a.ToBytes());
+            Assert.AreEqual(stream.Length, expectedByteLength);
+
+            stream.Seek(0, SeekOrigin.Begin);
+            ulong b = VarInt.FromStream(stream);
+            b.Should().Be(a).And.Be(value);
+        }
+    }
+}

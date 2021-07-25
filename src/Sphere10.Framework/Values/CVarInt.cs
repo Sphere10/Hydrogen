7d22c123d0757d9d207fce345b2dc79324975a23
hamish-rose <hamishr@protonmail.com>
2021-07-25 14:27:07 +1200

    SP10 Framework: CVarInt Write method and constructor from bytes
    now a readonly struct, ctor from bytes, and write directly to stream.

--- src/Sphere10.Framework/Values/CVarInt.cs
+++ src/Sphere10.Framework/Values/CVarInt.cs
@@ -6,7 +6,7 @@ namespace Sphere10.Framework.Values {
 	//Compact Variable integer - LEB128 compression based on NBitcoin CompactVarInt. Useful for 
 	// serializing small integers in a single byte. Larger integers may also be encoded but will be 
 	// less efficient than other formats.
-	public class CVarInt {
+	public readonly struct CVarInt  {
 		private readonly ulong _value;
 		private readonly int _size;
 		
@@ -15,13 +15,31 @@ namespace Sphere10.Framework.Values {
 			_size = size;
 		}
 
+		public CVarInt(byte[] bytes, int size) {
+			ulong n = 0;
+			
+			for (int i = 0; i < bytes.Length; i++) {
+				var chData = bytes[i];
+				var a = n << 7;
+				var b = (byte)(chData & 0x7F);
+				n = (a | b);
+				if ((chData & 0x80) != 0)
+					n++;
+				else
+					break;
+			}
+			
+			_value = n;
+			_size = size;
+		} 
+
 		/// <summary>
 		/// Read <see cref="CVarInt"/> from stream.
 		/// </summary>
 		/// <param name="size"> value size e.g. sizeof(uint)</param>
 		/// <param name="stream"> stream to read value from</param>
 		/// <returns> cvarint</returns>
-		public static CVarInt FromStream(int size, Stream stream) {
+		public static CVarInt Read(int size, Stream stream) {
 			ulong n = 0;
 			while (true)
 			{
@@ -38,13 +56,12 @@ namespace Sphere10.Framework.Values {
 		}
 
 		/// <summary>
-		/// encodes the cvarint value as bytes.
+		/// write cvarint value as bytes to stream.
 		/// </summary>
-		/// <returns></returns>
-		public byte[] ToBytes() {
+		/// <param name="stream"></param>
+		public void Write(Stream stream) {
 			var n = _value;
 			Span<byte> tmp = stackalloc byte[(_size * 8 + 6) / 7];
-			var builder = new ByteArrayBuilder();
 
 			var len = 0;
 			while (true)
@@ -58,11 +75,18 @@ namespace Sphere10.Framework.Values {
 				len++;
 			}
 			do
-			{
-				builder.Append(tmp[len]);
-			} while (len-- != 0);
+				stream.WriteByte(tmp[len]);
+			while (len-- != 0);
+		}
 
-			return builder.ToArray();
+		/// <summary>
+		/// encodes the cvarint value as bytes.
+		/// </summary>
+		/// <returns></returns>
+		public byte[] ToBytes() {
+			using var stream = new MemoryStream();
+			Write(stream);
+			return stream.ToArray();
 		}
 		
 		public static implicit operator ulong(CVarInt v) => v._value;
--- tests/Sphere10.Framework.Tests/Values/CVarIntTests.cs
+++ tests/Sphere10.Framework.Tests/Values/CVarIntTests.cs
@@ -18,11 +18,29 @@ namespace Sphere10.Framework.Tests.Values
         {
             var stream = new MemoryStream();
             var a = new CVarInt(value, size);
-            stream.Write(a.ToBytes());
+            a.Write(stream);
             Assert.AreEqual(expectedByteLength, stream.Length);
 
             stream.Seek(0, SeekOrigin.Begin);
-            ulong b = CVarInt.FromStream(size, stream);
+            ulong b = CVarInt.Read(size, stream);
+            b.Should().Be(a).And.Be(value);
+        }
+        
+        [Test]
+        [TestCase(ushort.MinValue, sizeof(ushort), 1)]
+        [TestCase((ulong)0x7F, sizeof(ushort), 1)]
+        [TestCase((ulong)0xFF, sizeof(ushort), 2)]
+        [TestCase(ushort.MaxValue, sizeof(ushort), 3)]
+        [TestCase(uint.MaxValue, sizeof(uint), 5)]
+        [TestCase(ulong.MaxValue, sizeof(ulong), 10)]
+        public void ToAndFromBytes(ulong value, int size, int expectedByteLength)
+        {
+            var stream = new MemoryStream();
+            var a = new CVarInt(value, size);
+            stream.Write(a.ToBytes());
+            Assert.AreEqual(expectedByteLength, stream.Length);
+            
+            ulong b = new CVarInt(stream.ToArray(), size);
             b.Should().Be(a).And.Be(value);
         }
     }

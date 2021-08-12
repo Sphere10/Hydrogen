38be7eb12043cacaae94db40b293f0f7a10241d5
David Price <davidsprice@yahoo.com>
2021-08-12 17:39:22 +1000

    add actions

---
+++ src/Sphere10.Hydrogen.Presentation2/UI/Controls/BlazorGrid/Classes/GridAction.cs
@@ -0,0 +1,20 @@
+﻿namespace Sphere10.Hydrogen.Presentation2.UI.Controls.BlazorGrid.Classes {
+	public class GridAction<TItem> 
+	{
+		public delegate TItem PerformAction(TItem item);
+		public delegate bool IsActionAvailableDelegate(TItem item);
+
+		public string Name { get; init; }
+		public PerformAction ActionWork { get; init; }
+		public string IconURL { get; init; }
+		public IsActionAvailableDelegate IsActionAvailable { get; init; }
+
+		public GridAction(string name, PerformAction actionWork, string iconURL, IsActionAvailableDelegate isActionAvailable = null) 
+		{
+			Name = name;
+			ActionWork = actionWork;
+			IconURL = iconURL;
+			IsActionAvailable = isActionAvailable;
+		}
+	}
+}

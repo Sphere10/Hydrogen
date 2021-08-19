689a2b12d8720a92f8b86d1301003c13d7636dfd
Herman Schoenfeld <herman@sphere10.com>
2021-08-19 15:37:43 +1000

    Added non-generic versions of generic parse methods

--- src/Sphere10.Framework/Text/GenericParser.cs
+++ src/Sphere10.Framework/Text/GenericParser.cs
@@ -3,7 +3,7 @@
 namespace Sphere10.Framework {
     public static class GenericParser {
 
-        public static bool TryParse<T>(this string input, out T value) {
+        public static bool TryParse<T>(string input, out T value) {
             // Special case: when input is empty and T is nullable, then parsed correctly as null
             if (input == string.Empty && typeof(T).IsGenericType) {
                 value = default;
@@ -22,7 +22,7 @@ namespace Sphere10.Framework {
             return false;
         }
 
-        public static T Parse<T>(this string input) {
+        public static T Parse<T>(string input) {
             if (!TryParse<T>(input, out var value))
                 throw new FormatException((input == null ? "Null string" : $"String '{input}'") + $" could not be parsed into an {typeof(T).Name}");
             return value;
@@ -33,6 +33,35 @@ namespace Sphere10.Framework {
             return value;
         }
 
+        public static bool TryParse(Type type, string input, out object value) {
+	        // Special case: when input is empty and T is nullable, then parsed correctly as null
+	        if (input == string.Empty && type.IsGenericType) {
+		        value = default;
+		        return true;
+	        }
+
+	        // Note: that NULL case is handled by nullable converter            
+
+	        // Use component model type convertors
+	        var converter = TypeDescriptorEx.GetConverter(typeof(T));
+	        if (converter != null && converter.IsValid(input)) {
+		        value = converter.ConvertFromString(input);
+		        return true;
+	        }
+	        value = default;
+	        return false;
+        }
+
+        public static object Parse(Type type, string input) {
+	        if (!TryParse(type,input, out var value))
+		        throw new FormatException((input == null ? "Null string" : $"String '{input}'") + $" could not be parsed into an {typeof(T).Name}");
+	        return value;
+        }
+
+        public static object SafeParse(Type type, string input) {
+	        TryParse(type, input, out var value);
+	        return value;
+        }
 
     }
 }
--- src/Sphere10.Framework/Text/ParseTool.cs
+++ src/Sphere10.Framework/Text/ParseTool.cs
@@ -19,15 +19,24 @@ namespace Tools {
 
 	public static class Parser {
 
-        public static bool TryParse<T>(this string input, out T value) 
+        public static bool TryParse<T>(string input, out T value) 
             => GenericParser.TryParse(input, out value);
         
-        public static T Parse<T>(this string input)
+        public static T Parse<T>(string input)
             => GenericParser.Parse<T>(input);
 
-        public static T SafeParse<T>(this string input)
+        public static T SafeParse<T>(string input)
             => GenericParser.SafeParse<T>(input);
 
-    }
+        public static bool TryParse(Type type, string input, out object value)
+	        => GenericParser.TryParse(type, input, out value);
+
+        public static object Parse(Type type, string input)
+	        => GenericParser.Parse(type, input);
+
+        public static object SafeParse(Type type, string input)
+	        => GenericParser.SafeParse(type, input);
+
+	}
 
 }

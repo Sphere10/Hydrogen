bd7bc8f2a18be689dd0a3c77d5aac1113ca986be
cobanicuss <79815312+cobanicuss@users.noreply.github.com>
2021-08-24 01:06:51 +1000

    Working on integration/unit testing the LocalQueue.

--- tests/Sphere10.Helium.Tests/Queue/HeliumLocalQueueTests.cs
+++ tests/Sphere10.Helium.Tests/Queue/HeliumLocalQueueTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using NUnit.Framework;
 using Sphere10.Framework;
 using Sphere10.Helium.Message;
@@ -8,6 +9,9 @@ using Sphere10.Helium.Queue;
 
 namespace Sphere10.Helium.Tests.Queue
 {
+    /// <summary>
+    /// INTEGRATION-TEST: Testing across inputs and outputs of methods.
+    /// </summary>
     [TestFixture]
     public class HeliumLocalQueueTests
     {
@@ -15,6 +19,7 @@ namespace Sphere10.Helium.Tests.Queue
         private static int _messageCommitEventCount;
         private LocalQueueSettings _localQueueSettings;
         private const int MessageBatchSize = 4;
+        private const int FiresOnlyOncePerCommit = 1;
 
         [SetUp]
         public void InitializeLocalHeliumQueue()
@@ -26,16 +31,62 @@ namespace Sphere10.Helium.Tests.Queue
             if (_localQueue.RequiresLoad)
                 _localQueue.Load();
 
-            FlushLocalQueueBeforeTesting();
-
             _localQueue.Committed += MessageAdded;
         }
 
         [Test]
         public void AddMessagesSynchronouslyToQueue()
         {
-            var messageList = GetMessageList();
+            FlushLocalQueue();
+            _messageCommitEventCount = 0;
 
+            var messageList = GetMessageList();
+            AddMessagesToQueue(messageList);
+
+            Assert.AreEqual(_messageCommitEventCount, FiresOnlyOncePerCommit);
+            Assert.AreEqual(_localQueue.Count, MessageBatchSize);
+
+            _messageCommitEventCount = 0;
+            FlushLocalQueue();
+
+            var messageCount = _localQueue.Count;
+            Assert.AreEqual(messageCount, 0);
+        }
+        
+
+        [Test]
+        public void RemoveMessagesSynchronouslyFromQueue()
+        {
+            FlushLocalQueue();
+            _messageCommitEventCount = 0;
+
+            var messageList = GetMessageList();
+            var enumMessageList = messageList.ToList();
+
+            AddMessagesToQueue(enumMessageList);
+
+            Assert.AreEqual(_localQueue.Count, enumMessageList.Count);
+            Assert.AreEqual(_messageCommitEventCount, FiresOnlyOncePerCommit);
+
+            RemoveMessagesFromQueue();
+
+            Assert.AreEqual(_localQueue.Count, 0);
+            Assert.AreEqual(_messageCommitEventCount, FiresOnlyOncePerCommit);
+
+            _messageCommitEventCount = 0;
+        }
+
+        private void RemoveMessagesFromQueue()
+        {
+            var messageCount = _localQueue.Count;
+
+            for (var i = 0; i < messageCount; i++) {
+                var message = _localQueue.RemoveMessage(); 
+            }
+        }
+
+        private void AddMessagesToQueue(IEnumerable<IMessage> messageList)
+        {
             using var txnScope = new FileTransactionScope(_localQueueSettings.TempDirPath);
             txnScope.BeginTransaction();
             txnScope.EnlistFile(_localQueue, false);
@@ -47,16 +98,6 @@ namespace Sphere10.Helium.Tests.Queue
             }
 
             txnScope.Commit();
-
-            Assert.AreEqual(_messageCommitEventCount, 1);
-            Assert.AreEqual(_localQueue.Count, MessageBatchSize);
-        }
-
-        [Test]
-        public void ReadMessageFromQueue()
-        {
-            var message = _localQueue.ReadMessage();
-            Assert.IsNotNull(message);
         }
 
         private static IMessage CreateMessage(string messageId)
@@ -86,7 +127,7 @@ namespace Sphere10.Helium.Tests.Queue
             _localQueue = new LocalQueue(_localQueueSettings);
         }
 
-        private void FlushLocalQueueBeforeTesting()
+        private void FlushLocalQueue()
         {
             var messageCount = _localQueue.Count;
             if (messageCount > 0) _localQueue.Clear();
@@ -98,7 +139,7 @@ namespace Sphere10.Helium.Tests.Queue
             _messageCommitEventCount++;
         }
 
-        private IList<IMessage> GetMessageList()
+        private static IEnumerable<IMessage> GetMessageList()
         {
             IList<IMessage> messageList = new List<IMessage>();
 

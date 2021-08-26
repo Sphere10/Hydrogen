46f9f22239bf3828ecddae5f8a62b514ae3c028a
David Price <davidsprice@yahoo.com>
2021-08-26 18:16:46 +1000

    Start column definitions Dynamic components

---
+++ src/Sphere10.Hydrogen.Presentation2/UI/Controls/BlazorGrid/Classes/IGridComponent.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Sphere10.Hydrogen.Presentation2.UI.Controls.BlazorGrid.Classes {
+	public interface IGridComponent 
+	{
+		public void Render(RenderTreeBuilder builder);
+	}
+}
---
+++ src/Sphere10.Hydrogen.Presentation2/UI/Controls/BlazorGrid/Components/Component2Test.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Sphere10.Hydrogen.Presentation2.UI.Controls.BlazorGrid.Classes;
+
+namespace Sphere10.Hydrogen.Presentation2.UI.Controls.BlazorGrid.Components {
+	partial class Component2Test : IGridComponent 
+	{
+		[Parameter] public bool Checked { get; set; }
+
+		public Component2Test() {}
+
+		public Component2Test(bool checkedValue) 
+		{
+			Checked = checkedValue;
+		}
+
+		public void Render(RenderTreeBuilder builder) 
+		{
+			builder.OpenComponent<Component2Test>(0);
+			builder.AddAttribute(1, "Checked", Checked);
+			builder.CloseComponent();
+		}
+	}
+}

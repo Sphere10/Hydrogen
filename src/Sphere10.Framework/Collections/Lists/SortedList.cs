82b5e93ab5eda493a410e4f9b5d71fb0b4f63239
Herman Schoenfeld <herman@sphere10.com>
2021-12-05 13:48:22 +1000

    SortedList collection fix and tests

--- src/Sphere10.Framework/Collections/Lists/SortedList.cs
+++ src/Sphere10.Framework/Collections/Lists/SortedList.cs
@@ -20,16 +20,18 @@ namespace Sphere10.Framework {
 	public class SortedList<T> : CollectionDecorator<T>, IReadOnlyList<T> {
 		private readonly IComparer<T> _comparer;
 		
-		public SortedList() : this(new List<T>()) {
+		public SortedList(SortDirection sortDirection = SortDirection.Ascending, IComparer<T> comparer = null) : this(new List<T>(), sortDirection, comparer) {
 		}
 
 		public SortedList(IList<T> internalList, SortDirection sortDirection = SortDirection.Ascending,  IComparer<T> comparer = null)
 			: base(internalList) {
 			Guard.ArgumentNotNull(internalList, nameof(internalList));
-			Guard.Argument(sortDirection != SortDirection.None, nameof(sortDirection), "Must be Ascending or Descending");
-			_comparer = comparer ?? Comparer<T>.Default;
-			if (sortDirection == SortDirection.Descending)
-				_comparer = new ReverseComparer<T>(_comparer);
+			comparer ??= Comparer<T>.Default;
+			_comparer = sortDirection switch {
+				SortDirection.Ascending => comparer,
+				SortDirection.Descending => new ReverseComparer<T>(comparer),
+				_ => throw new ArgumentException("Must be Ascending or Descending (or null)", nameof(sortDirection))
+			};
 		}
 
 		protected IList<T> InternalList => (IList<T>)base.InternalCollection;
--- tests/Sphere10.Framework.Tests/Collections/SortedListTests.cs
+++ tests/Sphere10.Framework.Tests/Collections/SortedListTests.cs
@@ -1,18 +1,28 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 
-namespace Sphere10.Framework.Tests
-{
-    public class SortedListTests
-    {
-        [Test]
-        [Ignore("Failing")]
-        public void SortIntsDefaultComparer()
-        {
-            var sortedList = new SortedList<int>();
-            sortedList.AddRangeSequentially(new [] {5, 4, 3, 2, 1});
+namespace Sphere10.Framework.Tests {
 
-            sortedList.Should().BeInAscendingOrder();
-        }
-    }
+	[Parallelizable]
+	public class SortedListTests {
+		[Test]
+		public void SortInts_Ascending_Default() {
+			SortInts_Ascending();
+		}
+
+		[Test]
+		public void SortInts_Ascending() {
+			var sortedList = new SortedList<int>(SortDirection.Ascending);
+			sortedList.AddRangeSequentially(new[] { 4, 5, 1, 3, 1, 2, 5 });
+			sortedList.Should().BeEquivalentTo(new[] { 1, 1, 2, 3, 4, 5, 5  });
+		}
+
+
+		[Test]
+		public void SortInts_Descending() {
+			var sortedList = new SortedList<int>(SortDirection.Descending);
+			sortedList.AddRangeSequentially(new[] { 4, 5, 1, 3, 1, 2, 5 });
+			sortedList.Should().BeEquivalentTo(new[] { 5, 5, 4, 3, 2, 1, 1 });
+		}
+	}
 }

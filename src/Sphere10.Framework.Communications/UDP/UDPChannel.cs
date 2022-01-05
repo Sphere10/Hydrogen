59299be5f93ebda2270ac824a0f3bce7cc315d71
David Price <davidsprice@yahoo.com>
2022-01-05 17:38:13 +1000

    Initial Channels

--- src/Sphere10.Framework.Communications/UDP/UDPChannel.cs
+++ src/Sphere10.Framework.Communications/UDP/UDPChannel.cs
@@ -6,39 +6,74 @@ using System.Threading.Tasks;
 
 namespace Sphere10.Framework.Communications {
 	public class UDPChannel : ProtocolChannel, IDisposable {
-
-		private IPEndPoint _remoteEndpoint;
-		private UdpClient _client;
+		private readonly UdpClient _client;
 
 		public UDPChannel(IPEndPoint localEndpoint, IPEndPoint remoteEndpoint, CommunicationRole role) {
-			_client = new UdpClient(localEndpoint);
-			_remoteEndpoint = remoteEndpoint;
+
+			if (localEndpoint.Address == IPAddress.Broadcast) {
+
+				var localHost = Dns.GetHostEntry(Dns.GetHostName());
+
+				var myIPAddress = string.Empty;
+				foreach (var ipAddress in localHost.AddressList) {
+					if (ipAddress.AddressFamily == AddressFamily.InterNetworkV6) continue;
+
+					var ipString = ipAddress.ToString();
+					var addressFamiliy = ipAddress.AddressFamily;
+
+					if (ipString.Contains("192.168")) {
+						myIPAddress = ipString;
+					}
+				}
+
+				_client = new UdpClient();
+//				_client.Client.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), localEndpoint.Port));
+				_client.Client.Bind(new IPEndPoint(IPAddress.Parse(myIPAddress), localEndpoint.Port));
+			} 
+			else { 
+				_client = new UdpClient(localEndpoint);
+			}
+
+	//		if (remoteEndpoint.Address == IPAddress.Broadcast) {
+	//			remoteEndpoint.Address = IPAddress.Any;
+	//		}
+
+			LocalEndpoint = localEndpoint;
+			RemoteEndpoint = remoteEndpoint;
 			LocalRole = role;
 		}
 
 		public override CommunicationRole LocalRole { get; }
 
+		public IPEndPoint LocalEndpoint { get; }
+
+		public IPEndPoint RemoteEndpoint { get; }
+
 		protected override async Task OpenInternal() {
-			// UDP has no concept of opening the connection
-			_client.Connect(_remoteEndpoint);
+			_client?.Connect(RemoteEndpoint);
+SystemLog.Info($"UDPChannel Opened Listening On {LocalEndpoint.Address}:{LocalEndpoint.Port}  to  {RemoteEndpoint.Address}:{RemoteEndpoint.Port}");
 		}
 
 		protected override async Task CloseInternal() {
-			// UDP has no concept of closing the connection
-			_client.Close();
+			_client?.Close();
+SystemLog.Info("UDPChannel Closed");
 		}
 
 		protected override async Task<byte[]> ReceiveBytesInternal(CancellationToken cancellationToken) {
+//SystemLog.Info("UDPChannel About to Block Waiting for Connection");
 			var result = await _client.ReceiveAsync().WithCancellationToken(cancellationToken);
+//SystemLog.Info("UDPChannel Received Data");
+
 			return result.Buffer;
 		}
 
 		protected override async Task<bool> TrySendBytesInternal(ReadOnlyMemory<byte> bytes, CancellationToken cancellationToken) {
 			try {
+SystemLog.Info($"UDP Channel Sending to: {RemoteEndpoint.Address} Port: {RemoteEndpoint.Port}");
 				var sentLength = await _client.SendAsync(bytes.ToArray(), bytes.Length).WithCancellationToken(cancellationToken);
 				if (sentLength != bytes.Length)
 					return false;
-			} catch (Exception error) {
+			} catch (Exception ex) {
 				return false;
 			}
 			return true;

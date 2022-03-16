531134f9b5a2a11b1fdf02168cbe3dd35b688cd6
Herman Schoenfeld <herman@sphere10.com>
2022-03-17 00:10:04 +1000

    Minor refactoring IMerkleizedCollection

---
+++ src/Sphere10.Framework/Collections/Merkle/IMerkleizedCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Sphere10.Framework {
+
+	public interface IMerkleizedCollection<TItem> : ICollection<TItem> {
+		IMerkleTree MerkleTree { get; }
+	}
+
+}
--- src/Sphere10.Framework/Collections/Merkle/IMerklizedCollection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sphere10.Framework {
-
-	public interface IMerklizedCollection<TItem> : IExtendedCollection<TItem> {
-		IMerkleTree MerkleTree { get; }
-	}
-
-}

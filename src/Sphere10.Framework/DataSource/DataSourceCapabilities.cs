f0277522615d7ca76032f63454fb444431d7f524
Herman Schoenfeld <herman@sphere10.com>
2022-04-21 17:56:09 +1000

    Added DataSourceCapabilities

--- src/Sphere10.Framework/DataSource/DataSourceCapabilities.cs
+++ src/Sphere10.Framework/DataSource/DataSourceCapabilities.cs
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------
-// <copyright file="ICrudDataSource.cs" company="Sphere 10 Software">
+// <copyright file="CrudCapabilities.cs" company="Sphere 10 Software">
 //
 // Copyright (c) Sphere 10 Software. All rights reserved. (http://www.sphere10.com)
 //
@@ -11,18 +11,22 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Sphere10.Framework {
-
-	public interface ICrudDataSource<TEntity>  {
-		TEntity New();
-		void Create(TEntity entity);
-		IEnumerable<TEntity> Read(string searchTerm, int pageLength, ref int page, string sortProperty, SortDirection sortDirection, out int totalItems);
-		TEntity Refresh(TEntity entity);
-		void Update(TEntity entity);
-		void Delete(TEntity entity);
-		IEnumerable<string> Validate(TEntity entity, CrudAction action);
+	[Flags]
+	public enum DataSourceCapabilities {
+		CanCreate	= 1 << 0,
+		CanRead		= 1 << 1,
+		CanUpdate	= 1 << 2 | CanRead,
+		CanDelete	= 1 << 3,
+		CanSearch	= 1 << 4 | CanRead,
+		CanSort		= 1 << 5 | CanRead,
+		CanPage		= 1 << 6 | CanRead,
+		Default = CanCreate | CanRead | CanUpdate | CanDelete | CanSearch | CanSort | CanPage,
+		ComboBoxDefault = CanRead | CanSearch | CanSort
 	}
-
 }

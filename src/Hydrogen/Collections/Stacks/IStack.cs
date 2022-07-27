4e1231ba2ddf956692c042394eb24ce3a38a4939
Herman Schoenfeld <herman@sphere10.com>
2022-07-27 23:16:35 +1000

    Added Stack abstraction to Hydrogen/Collections/Stacks

---
+++ src/Hydrogen/Collections/Stacks/IStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hydrogen;
+
+public interface IStack<T> : ICollection<T> {
+
+	public bool TryPeek(out T value);
+
+	public bool TryPop(out T value);
+
+	public void Push(T item);
+
+}
+
+
+public static class IStackExtensions {
+	public static T Pop<T>(this IStack<T> stack) {
+		Guard.Ensure(stack.Count > 0, "Insufficient items");
+		if (!stack.TryPop(out var value))
+			throw new InvalidOperationException("Unable to pop from stack");
+		return value;
+	}
+
+	public static T Peek<T>(this IStack<T> stack) {
+		Guard.Ensure(stack.Count > 1, "Insufficient items");
+		if (!stack.TryPeek(out var value))
+			throw new InvalidOperationException("Unable to pop from stack");
+		return value;
+	}
+}
---
+++ src/Hydrogen/Collections/Stacks/StackList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Resources;
+using System.Text;
+using System.Linq;
+
+namespace Hydrogen;
+
+public class StackList<T> : ExtendedListDecorator<T>, IStack<T> {
+
+	public StackList() : this(new ExtendedList<T>()) {
+	}
+
+	public StackList(IExtendedList<T> internalExtendedList) : base(internalExtendedList) {
+	}
+	
+	public bool TryPeek(out T value) => TryPeek(out value, 1);
+
+	public bool TryPeek(out T value, int depth) {
+		var count = Count;
+		var itemIX = count - depth;
+		if (itemIX < 0) {
+			value = default(T);
+			return false;
+		}
+		value = base.Read(itemIX);
+		return true;
+	}
+
+
+	public bool TryPop(out T value) {
+		var count = Count;
+		if (count <= 0) {
+			value = default(T);
+			return false;
+		}
+		value = base.Read(count - 1);
+		base.RemoveAt(count - 1);
+		return true;
+	}
+
+	
+	public T Peek(int depth) {
+		if (!TryPeek(out var value, depth))
+			throw new InvalidOperationException($"Unable to peek stack at depth {depth}");
+		return value;
+	}
+
+	public void Push(T item) => base.Add(item);
+}
+

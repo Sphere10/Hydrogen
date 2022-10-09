582802d1f1ce4f4a4d0e0e6796e9672ef4e1146e
Herman Schoenfeld <herman@sphere10.com>
2022-10-09 17:11:35 +1000

    Dependency bump

--- tests/Hydrogen.Tests/Misc/NetFrameworkStandardBehaviour.cs
+++ tests/Hydrogen.Tests/Misc/NetFrameworkStandardBehaviour.cs
@@ -17,7 +17,6 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Diagnostics.Runtime.Interop;
 using NUnit.Framework;
 
 namespace Hydrogen.Tests {

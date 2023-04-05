b6f6c7f8f3eec0884a9838a19f85e82b90c8edac
Herman Schoenfeld <herman@sphere10.com>
2023-04-05 18:04:20 +1000

    Cosmetic fixes: - Resize options screen to fit on high-dpi screens - Application Bar icon fits properly on high-dpi screen

--- src/Hydrogen.Windows.Forms/Application/Controls/ApplicationBanner.cs
+++ src/Hydrogen.Windows.Forms/Application/Controls/ApplicationBanner.cs
@@ -18,31 +18,36 @@ using Hydrogen.Application;
 using Microsoft.Extensions.DependencyInjection;
 
 
-namespace Hydrogen.Windows.Forms {
-	public partial class ApplicationBanner : ApplicationControl {
+namespace Hydrogen.Windows.Forms
+{
+	public partial class ApplicationBanner : ApplicationControl
+	{
 		private string _companyName;
 		private string _version;
 		private string _title;
 
 		public ApplicationBanner()
-			: base() {
+			: base()
+		{
 			InitializeComponent();
 
 			// Download child control defaults as defaults at construction
 			CompanyName = _companyNameLabel.Text;
 			Version = _versionLabel.Text;
 			Title = _productNameLabel.Text;
-			ApplicationIcon = Tools.Values.Future.LazyLoad( () => HydrogenFramework.Instance.ServiceProvider.GetService<IApplicationIconProvider>().ApplicationIcon);
+			ApplicationIcon = Tools.Values.Future.LazyLoad(() => HydrogenFramework.Instance.ServiceProvider.GetService<IApplicationIconProvider>().ApplicationIcon);
 		}
 
 		protected IFuture<Icon> ApplicationIcon { get; }
-		protected override void OnLoad(EventArgs e) {
+		protected override void OnLoad(EventArgs e)
+		{
 			base.OnLoad(e);
 		}
 
-		protected override void InitializeUIPrimingData() {
+		protected override void InitializeUIPrimingData()
+		{
 			base.InitializeUIPrimingData();
-			_iconPanel.BackgroundImage = ApplicationIcon.Value.ToBitmap().Resize(new Size(_iconPanel.Width, _iconPanel.Height), ResizeMethod.AspectFit);
+			_iconPanel.BackgroundImage = ApplicationIcon.Value.ToBitmap().Resize(new Size(_iconPanel.Width, _iconPanel.Height), ResizeMethod.AspectFitPadded, paddingColor: Color.Transparent);
 			// Upload to child controls the processed strings. Visual inheritance can be used to change these strings.
 			_companyNameLabel.Text = Hydrogen.StringFormatter.FormatEx(CompanyName);
 			_productNameLabel.Text = Hydrogen.StringFormatter.FormatEx(Title);
@@ -52,14 +57,18 @@ namespace Hydrogen.Windows.Forms {
 
 		[Category("Appearance")]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-		public new string CompanyName {
+		public new string CompanyName
+		{
 			get { return _companyName; }
-			set {
+			set
+			{
 				_companyName = value;
-				if (Tools.Runtime.IsDesignMode) {
+				if (Tools.Runtime.IsDesignMode)
+				{
 					_companyNameLabel.Text = _companyName;
 				}
-				if (Loaded) {
+				if (Loaded)
+				{
 					_companyNameLabel.Text = Hydrogen.StringFormatter.FormatEx(_companyName);
 				}
 				Invalidate();
@@ -68,14 +77,18 @@ namespace Hydrogen.Windows.Forms {
 
 		[Category("Appearance")]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-		public string Version {
+		public string Version
+		{
 			get { return _version; }
-			set {
+			set
+			{
 				_version = value;
-				if (Tools.Runtime.IsDesignMode) {
+				if (Tools.Runtime.IsDesignMode)
+				{
 					_versionLabel.Text = _version;
 				}
-				if (Loaded) {
+				if (Loaded)
+				{
 					_versionLabel.Text = Hydrogen.StringFormatter.FormatEx(_version);
 				}
 				Invalidate();
@@ -84,14 +97,18 @@ namespace Hydrogen.Windows.Forms {
 
 		[Category("Appearance")]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-		public string Title {
+		public string Title
+		{
 			get { return _title; }
-			set {
+			set
+			{
 				_title = value;
-				if (Tools.Runtime.IsDesignMode) {
+				if (Tools.Runtime.IsDesignMode)
+				{
 					_productNameLabel.Text = _title;
 				}
-				if (Loaded) {
+				if (Loaded)
+				{
 					_productNameLabel.Text = Hydrogen.StringFormatter.FormatEx(_title);
 				}
 				Invalidate();
@@ -100,9 +117,11 @@ namespace Hydrogen.Windows.Forms {
 
 		[Category("Appearance")]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-		public Color FromColor {
+		public Color FromColor
+		{
 			get { return _gradientPanel.FromColor; }
-			set {
+			set
+			{
 				_gradientPanel.FromColor = value;
 				Invalidate();
 			}
@@ -110,16 +129,20 @@ namespace Hydrogen.Windows.Forms {
 
 		[Category("Appearance")]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-		public Color ToColor {
+		public Color ToColor
+		{
 			get { return _gradientPanel.ToColor; }
-			set {
+			set
+			{
 				_gradientPanel.ToColor = value;
 				Invalidate();
 			}
 		}
 
-		private void _iconPanel_SizeChanged(object sender, EventArgs e) {
-			if (Loaded) {
+		private void _iconPanel_SizeChanged(object sender, EventArgs e)
+		{
+			if (Loaded)
+			{
 				_iconPanel.BackgroundImage = ApplicationIcon.Value.ToBitmap(_iconPanel.Width, _iconPanel.Height);
 			}
 		}

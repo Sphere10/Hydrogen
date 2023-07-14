7373ef8e5bd2fc452e19bd252e0deca683596519
Herman Schoenfeld <herman@sphere10.com>
2023-07-14 15:00:22 +1000

    Refactored Base32Encoding

--- src/Hydrogen/Encoding/Base32Encoding.cs
+++ src/Hydrogen/Encoding/Base32Encoding.cs
@@ -14,7 +14,7 @@ namespace Hydrogen;
 public sealed class Base32Encoding {
 
 	// the valid chars for the encoding
-	private static string ValidChars = "QAZ2WSX3" + "EDC4RFV5" + "TGB6YHN7" + "UJM8K9LP";
+	private static readonly string ValidChars = "QAZ2WSX3" + "EDC4RFV5" + "TGB6YHN7" + "UJM8K9LP";
 
 
 	public static string Encode(string asciiString) {
@@ -25,30 +25,35 @@ public sealed class Base32Encoding {
 	/// <summary>
 	/// Converts an array of bytes to a Base32-k string.
 	/// </summary>
-	public static string Encode(byte[] bytes) {
-		StringBuilder sb = new StringBuilder(); // holds the base32 chars
-		byte index;
-		int hi = 5;
-		int currentByte = 0;
+	public static string Encode(ReadOnlySpan<byte> bytes) {
+		var sb = new StringBuilder(); // holds the base32 chars
+		var hi = 5;
+		var currentByte = 0;
 		while (currentByte < bytes.Length) {
 			// do we need to use the next byte?
-			if (hi > 8) {
-				// get the last piece from the current byte, shift it to the right
-				// and increment the byte counter
-				index = (byte)(bytes[currentByte++] >> (hi - 5));
-				if (currentByte != bytes.Length) {
-					// if we are not at the end, get the first piece from
-					// the next byte, clear it and shift it to the left
-					index = (byte)(((byte)(bytes[currentByte] << (16 - hi)) >> 3) | index);
+			byte index;
+			switch (hi) {
+				case > 8: {
+					// get the last piece from the current byte, shift it to the right
+					// and increment the byte counter
+					index = (byte)(bytes[currentByte++] >> (hi - 5));
+					if (currentByte != bytes.Length) {
+						// if we are not at the end, get the first piece from
+						// the next byte, clear it and shift it to the left
+						index = (byte)(((byte)(bytes[currentByte] << (16 - hi)) >> 3) | index);
+					}
+					hi -= 3;
+					break;
 				}
-				hi -= 3;
-			} else if (hi == 8) {
-				index = (byte)(bytes[currentByte++] >> 3);
-				hi -= 3;
-			} else {
-				// simply get the stuff from the current byte
-				index = (byte)((byte)(bytes[currentByte] << (8 - hi)) >> 3);
-				hi += 5;
+				case 8:
+					index = (byte)(bytes[currentByte++] >> 3);
+					hi -= 3;
+					break;
+				default:
+					// simply get the stuff from the current byte
+					index = (byte)((byte)(bytes[currentByte] << (8 - hi)) >> 3);
+					hi += 5;
+					break;
 			}
 			sb.Append(ValidChars[index]);
 		}
@@ -65,31 +70,27 @@ public sealed class Base32Encoding {
 		if (str == string.Empty)
 			return Array.Empty<byte>();
 
-		int numBytes = str.Length * 5 / 8;
-		byte[] bytes = new Byte[numBytes];
+		var numBytes = str.Length * 5 / 8;
+		var bytes = new byte[numBytes];
 
 		// all UPPERCASE chars
 		str = str.ToUpper();
-
-		int bit_buffer;
-		int currentCharIndex;
-		int bits_in_buffer;
 
 		if (str.Length < 3) {
 			bytes[0] = (byte)(ValidChars.IndexOf(str[0]) | ValidChars.IndexOf(str[1]) << 5);
 			return bytes;
 		}
 
-		bit_buffer = (ValidChars.IndexOf(str[0]) | ValidChars.IndexOf(str[1]) << 5);
-		bits_in_buffer = 10;
-		currentCharIndex = 2;
-		for (int i = 0; i < bytes.Length; i++) {
-			bytes[i] = (byte)bit_buffer;
-			bit_buffer >>= 8;
-			bits_in_buffer -= 8;
-			while (bits_in_buffer < 8 && currentCharIndex < str.Length) {
-				bit_buffer |= ValidChars.IndexOf(str[currentCharIndex++]) << bits_in_buffer;
-				bits_in_buffer += 5;
+		var bitBuffer = (ValidChars.IndexOf(str[0]) | ValidChars.IndexOf(str[1]) << 5);
+		var bitsInBuffer = 10;
+		var currentCharIndex = 2;
+		for (var i = 0; i < bytes.Length; i++) {
+			bytes[i] = (byte)bitBuffer;
+			bitBuffer >>= 8;
+			bitsInBuffer -= 8;
+			while (bitsInBuffer < 8 && currentCharIndex < str.Length) {
+				bitBuffer |= ValidChars.IndexOf(str[currentCharIndex++]) << bitsInBuffer;
+				bitsInBuffer += 5;
 			}
 		}
 

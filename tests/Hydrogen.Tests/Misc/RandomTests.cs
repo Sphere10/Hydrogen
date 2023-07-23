8ff7260fca5487dba33e2cda1eabb82bc5c8c4a1
Herman Schoenfeld <herman@sphere10.com>
2023-07-23 20:04:19 +1000

    Added (and commented out) a unit tet for random long generation

--- tests/Hydrogen.Tests/Misc/RandomTests.cs
+++ tests/Hydrogen.Tests/Misc/RandomTests.cs
@@ -7,6 +7,7 @@
 // This notice must not be removed when duplicating this file or its contents, in whole or in part.
 
 using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Hydrogen.Tests;
@@ -52,4 +53,50 @@ public class RandomTests {
 		Assert.AreEqual(0, range.End);
 	}
 
+	//[Test]
+	//public void RandomLong([Values(100000)] int iterations) {
+	//	var rng = new Random(31337);
+	//	var expected = Enumerable.Range(19, 8).Select(x => (long)x).ToArray();
+	//	var tally19 = 0L;
+	//	var tally20 = 0L;
+	//	var tally21 = 0L;
+	//	var tally22 = 0L;
+	//	var tally23 = 0L;
+	//	var tally24 = 0L;
+	//	var tally25 = 0L;
+	//	var tally26 = 0L;
+	//	for (var i = 0; i < iterations; i++) {
+	//		var actual = rng.NextLong(19, 27);
+	//		Assert.That(expected, Contains.Item(actual));
+	//		switch (actual) {
+	//			case 19:
+	//				tally19++;
+	//				break;
+	//			case 20:
+	//				tally20++;
+	//				break;
+	//			case 21:
+	//				tally21++;
+	//				break;
+	//			case 22:
+	//				tally22++;
+	//				break;
+	//			case 23:
+	//				tally23++;
+	//				break;
+	//			case 24:
+	//				tally24++;
+	//				break;
+	//			case 25:
+	//				tally25++;
+	//				break;
+	//			case 26:
+	//				tally26++;
+	//				break;
+	//			default:
+	//				throw new InvalidOperationException();
+	//		}
+
+	//	}
+	//}
 }

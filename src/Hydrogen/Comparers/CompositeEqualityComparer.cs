b00a236f52a56b8c5b0d2986ebcf1616f654ac08
Herman Schoenfeld <herman@sphere10.com>
2023-07-28 22:36:44 +1000

    Added CompositeEqualityComparer and IdempotentEqualityComparer

---
+++ src/Hydrogen/Comparers/CompositeEqualityComparer.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Sphere 10 Software. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT software license, see the accompanying file
+// LICENSE or visit http://www.opensource.org/licenses/mit-license.php.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hydrogen;
+
+public class CompositeEqualityComparer<T> : IEqualityComparer<T> {
+	private readonly IEqualityComparer<T> _primary;
+	private readonly IEqualityComparer<T> _secondary;
+
+	public CompositeEqualityComparer(IEqualityComparer<T> single) : this(single, IdempotentEqualityComparer<T>.Instance) {
+	}
+
+	public CompositeEqualityComparer(IEnumerable<IEqualityComparer<T>> comparers) 
+		: this(From(comparers)) {
+	}
+
+	public CompositeEqualityComparer(IEqualityComparer<T> primary, IEqualityComparer<T> secondary) {
+		Guard.ArgumentNotNull(primary, nameof(primary));
+		Guard.ArgumentNotNull(secondary, nameof(secondary));
+		_primary = primary;;
+		_secondary = secondary;
+	}
+
+	public bool Equals(T x, T y) => _primary.Equals(x, y) && _secondary.Equals(x, y);
+
+	public int GetHashCode(T obj) => HashCode.Combine(_primary.GetHashCode(obj), _secondary.GetHashCode(obj));
+
+	public static CompositeEqualityComparer<T> From(IEnumerable<IEqualityComparer<T>> comparers) {
+		Guard.ArgumentNotNull(comparers, nameof(comparers));
+		var comparersArr = comparers as IEqualityComparer<T>[] ?? comparers.ToArray();
+		switch(comparersArr.Count()) {
+			case 0: 
+				throw new ArgumentException("Must have at least one comparer", nameof(comparers));
+			case 1:
+				return new CompositeEqualityComparer<T>(comparersArr.Single());
+			case 2:
+				return new CompositeEqualityComparer<T>(comparersArr.Single(), comparersArr.Skip(1).Single());
+			default:
+				return new CompositeEqualityComparer<T>(comparersArr.First(), From(comparersArr.Skip(1)));
+		}
+		 
+	}
+}
---
+++ src/Hydrogen/Comparers/IdempotentEqualityComparer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Hydrogen;
+
+public class IdempotentEqualityComparer<T> : IEqualityComparer<T> {
+	public static readonly IdempotentEqualityComparer<T> Instance = new();
+
+	private IdempotentEqualityComparer() {
+	}
+
+	public bool Equals(T x, T y) => true;
+
+	public int GetHashCode(T obj) => 0;
+}

5bb1720275c1daf096730eafa571aff218c9c371
Herman Schoenfeld <herman@sphere10.com>
2023-07-28 22:38:35 +1000

    Added NegatedEqualityComparer

---
+++ src/Hydrogen/Comparers/NegatedEqualityComparer.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Sphere 10 Software. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT software license, see the accompanying file
+// LICENSE or visit http://www.opensource.org/licenses/mit-license.php.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+
+using System.Collections.Generic;
+
+namespace Hydrogen;
+
+public sealed class NegatedEqualityComparer<T> : IEqualityComparer<T> {
+	private readonly IEqualityComparer<T> _source;
+
+	public NegatedEqualityComparer(IEqualityComparer<T> source) {
+		_source = source;
+	}
+
+	public bool Equals(T x, T y) => !_source.Equals(x, y);
+
+	public int GetHashCode(T obj) => _source.GetHashCode(obj);
+}

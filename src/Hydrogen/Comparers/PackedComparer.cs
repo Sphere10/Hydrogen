7babbefbed6d5358ed5b10fc62b8b0151f606cf3
Herman Schoenfeld <herman@sphere10.com>
2023-07-29 20:43:11 +1000

    Added PackedComparer

---
+++ src/Hydrogen/Comparers/PackedComparer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Hydrogen;
+
+public class PackedComparer : IComparer<object> {
+	private readonly object _comparer;
+	private readonly IComparer<object> _projectedComparer;
+	
+	private PackedComparer(object comparer, IComparer<object> projectedComparer) {
+		Guard.ArgumentNotNull(comparer, nameof(comparer));
+		Guard.ArgumentNotNull(projectedComparer, nameof(projectedComparer));
+		Guard.Argument(comparer.GetType().IsSubtypeOfGenericType(typeof(IComparer<>)), nameof(projectedComparer), "Must be an IComparer<>");	
+		Guard.Argument(projectedComparer.GetType().IsSubtypeOfGenericType(typeof(ProjectedComparer<,>)), nameof(projectedComparer), "Must be an ProjectedComparer<>");	
+		_comparer = comparer;
+		_projectedComparer = projectedComparer;
+	}
+
+	public int Compare(object x, object y) => _projectedComparer.Compare(x, y);
+
+	public static PackedComparer Pack<TItem>(IComparer<TItem> comparer) {
+		Guard.ArgumentNotNull(comparer, nameof(comparer));
+		return new PackedComparer(comparer, comparer.AsProjection(x => (object)x, x => (TItem)x));
+	}
+
+	public IComparer<TItem> Unpack<TItem>() {
+		var unpacked = _comparer as IComparer<TItem>;
+		Guard.Ensure(unpacked != null, $"Cannot unpack {_comparer.GetType().Name} as is not an IComparer<{typeof(TItem).Name}>");
+		return unpacked;
+	}
+	
+}
---
+++ tests/Hydrogen.Tests/Comparers/PackedComparerTests.cs
@@ -0,0 +1,68 @@
+// Copyright (c) Sphere 10 Software. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT software license, see the accompanying file
+// LICENSE or visit http://www.opensource.org/licenses/mit-license.php.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Hydrogen.Tests;
+
+[TestFixture]
+[Parallelizable(ParallelScope.Children)]
+public class PackedComparerTests {
+
+	[Test]
+	public void TestPack() {
+		IComparer<string> stringComparer = StringComparer.InvariantCultureIgnoreCase;
+		Assert.That(()=> stringComparer.AsPacked(), Is.Not.Null);
+	}
+
+	[Test]
+	public void TestUnpack() {
+		IComparer<string> stringComparer = StringComparer.InvariantCultureIgnoreCase;
+		var packedComparer = PackedComparer.Pack(stringComparer);
+
+		var unpacked = packedComparer.Unpack<string>();
+
+		Assert.That(unpacked, Is.SameAs(stringComparer));
+	}
+
+	[Test]
+	public void TestUnpack_WrongTypeThrows() {
+		IComparer<string> stringComparer = StringComparer.InvariantCultureIgnoreCase;
+		var packedComparer = PackedComparer.Pack(stringComparer);
+		Assert.That(() => packedComparer.Unpack<int>(), Throws.InvalidOperationException);
+	}
+
+	[Test]
+	public void TestCompare_Consistency() {
+		IComparer<string> stringComparer = StringComparer.InvariantCultureIgnoreCase;
+		var packedComparer = stringComparer.AsPacked();
+		Assert.AreEqual(stringComparer.Compare(null, null), packedComparer.Compare(null, null));
+		Assert.AreEqual(stringComparer.Compare(null, "B"), packedComparer.Compare(null, "B"));
+		Assert.AreEqual(stringComparer.Compare("A", null), packedComparer.Compare("A", null));
+		Assert.AreEqual(stringComparer.Compare("a", "b"), packedComparer.Compare("a", "b"));
+		Assert.AreEqual(stringComparer.Compare("a", "a"), packedComparer.Compare("a", "a"));
+		Assert.AreEqual(stringComparer.Compare("a", "A"), packedComparer.Compare("a", "A"));
+	}
+
+	[Test]
+	public void TestCompareWrongTypeThrowsCastException_1() {
+		IComparer<string> stringComparer = StringComparer.InvariantCultureIgnoreCase;
+		var packedComparer = stringComparer.AsPacked();
+		Assert.That(() => packedComparer.Compare(1, 2), Throws.InstanceOf<InvalidCastException>());
+	}
+
+	[Test]
+	public void TestCompareWrongTypeThrowsCastException_2() {
+		IComparer<string> stringComparer = StringComparer.InvariantCultureIgnoreCase;
+		var packedComparer = stringComparer.AsPacked();
+		Assert.That(() => packedComparer.Compare("a", 2), Throws.InstanceOf<InvalidCastException>());
+	}
+
+}

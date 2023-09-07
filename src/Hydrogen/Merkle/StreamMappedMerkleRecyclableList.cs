e631dfb93030f71a93443309af6c11456241361e
Herman Schoenfeld <herman@sphere10.com>
2023-09-07 21:15:31 +1000

    Added StreamMappedMerkleRecyclableList ADT

---
+++ src/Hydrogen/Merkle/StreamMappedMerkleRecyclableList.cs
@@ -0,0 +1,137 @@
+﻿// Copyright (c) Sphere 10 Software. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT software license, see the accompanying file
+// LICENSE or visit http://www.opensource.org/licenses/mit-license.php.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+//
+// NOTE: This file is part of the reference implementation for Dynamic Merkle-Trees. Read the paper at:
+// Web: https://sphere10.com/tech/dynamic-merkle-trees
+// e-print: https://vixra.org/abs/2305.0087
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Hydrogen;
+
+public class StreamMappedMerkleRecyclableList<TItem> : RecyclableListDecorator<TItem, IStreamMappedRecyclableList<TItem>>, IStreamMappedRecyclableList<TItem>, IMerkleList<TItem> {
+
+	public event EventHandlerEx<object> Loading { add => InternalCollection.Loading += value; remove => InternalCollection.Loading -= value; }
+	public event EventHandlerEx<object> Loaded { add => InternalCollection.Loaded += value; remove => InternalCollection.Loaded -= value; }
+
+	private readonly ObjectContainerMerkleTree _merkleTreeIndex;
+
+	public StreamMappedMerkleRecyclableList(
+		Stream rootStream,
+		CHF hashAlgorithm,
+		int clusterSize,
+		IItemSerializer<TItem> itemSerializer = null,
+		IEqualityComparer<TItem> itemComparer = null,
+		IItemChecksummer<TItem> itemChecksummer = null,
+		StreamContainerPolicy policy = StreamContainerPolicy.Default,
+		long reservedStreams = 2,
+		long merkleTreeStreamIndex = 0,
+		long freeIndexStoreStreamIndex = 1,
+		long checksumIndexStreamIndex = 2,
+		Endianness endianness = Endianness.LittleEndian,
+		bool autoLoad = false
+	) : this(
+		CreateRecyclableList(
+			rootStream,
+			hashAlgorithm,
+			clusterSize,
+			itemSerializer,
+			itemComparer,
+			itemChecksummer,
+			policy,
+			reservedStreams,
+			freeIndexStoreStreamIndex,
+			checksumIndexStreamIndex,
+			merkleTreeStreamIndex,
+			endianness,
+			out var merkleTreeIndex
+		),
+		merkleTreeIndex,
+		autoLoad
+	) {
+	}
+	
+	internal StreamMappedMerkleRecyclableList(
+		IStreamMappedRecyclableList<TItem> streamMappedList,
+		ObjectContainerMerkleTree merkleTreeIndex,
+		bool autoLoad = false
+	) : base(streamMappedList) {
+		Guard.ArgumentNotNull(merkleTreeIndex, nameof(merkleTreeIndex));
+		_merkleTreeIndex = merkleTreeIndex;
+
+		if (autoLoad && RequiresLoad)
+			Load();
+	}
+
+	public ObjectContainer<TItem> ObjectContainer => InternalCollection.ObjectContainer;
+
+	public IItemSerializer<TItem> ItemSerializer => InternalCollection.ItemSerializer;
+
+	public IEqualityComparer<TItem> ItemComparer => InternalCollection.ItemComparer;
+
+	public IMerkleTree MerkleTree => _merkleTreeIndex.MerkleTree;
+
+	public bool RequiresLoad => InternalCollection.RequiresLoad;
+
+	public void Load() => InternalCollection.Load();
+
+	public Task LoadAsync() => InternalCollection.LoadAsync();
+
+	public void Dispose() {
+		InternalCollection.Dispose();
+	}
+
+	private static IStreamMappedRecyclableList<TItem> CreateRecyclableList(
+		Stream rootStream,
+		CHF hashAlgorithm,
+		int clusterSize,
+		IItemSerializer<TItem> itemSerializer,
+		IEqualityComparer<TItem> itemComparer,
+		IItemChecksummer<TItem> itemChecksummer,
+		StreamContainerPolicy policy,
+		long reservedStreams,
+		long freeIndexStoreStreamIndex,
+		long checksumIndexStreamIndex,
+		long merkleTreeIndexStreamIndex,
+		Endianness endianness,
+		out ObjectContainerMerkleTree merkleTreeIndex
+	) {
+		var streamMappedList = new StreamMappedRecyclableList<TItem>(
+			rootStream,
+			clusterSize,
+			itemSerializer,
+			itemComparer,
+			itemChecksummer,
+			policy,
+			reservedStreams,
+			freeIndexStoreStreamIndex,
+			checksumIndexStreamIndex,
+			endianness,
+			false
+		);
+
+		merkleTreeIndex = new ObjectContainerMerkleTree(
+			streamMappedList.ObjectContainer,
+			x => DigestItem(streamMappedList.ObjectContainer, x, hashAlgorithm),
+			hashAlgorithm,
+			merkleTreeIndexStreamIndex,
+			0
+		);
+		streamMappedList.ObjectContainer.RegisterMetaDataProvider(merkleTreeIndex);
+
+		return streamMappedList;
+	}
+
+	private static byte[] DigestItem(ObjectContainer container, long index, CHF chf) {
+		var bytes = container.GetItemBytes(index);
+		return Hashers.HashWithNullSupport(chf, bytes);
+	}
+
+}
---
+++ tests/Hydrogen.Tests/Collections/StreamMappedMerkle/StreamMappedMerkleRecyclableListTests.cs
@@ -0,0 +1,169 @@
+﻿// Copyright (c) Sphere 10 Software. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT software license, see the accompanying file
+// LICENSE or visit http://www.opensource.org/licenses/mit-license.php.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Hydrogen.NUnit;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Hydrogen.Tests;
+
+[TestFixture]
+[Parallelizable(ParallelScope.Children)]
+public class StreamMappedMerkleRecyclableListTests : RecyclableListTestsBase {
+
+	protected override IDisposable CreateList<T>(IItemSerializer<T> serializer, IEqualityComparer<T> comparer, out IRecyclableList<T> list) {
+		var stream = new MemoryStream();
+		var smrlist = new StreamMappedMerkleRecyclableList<T>(
+			stream,
+			CHF.SHA2_256,
+			32, 
+			serializer,
+			autoLoad: true
+		);
+		list = smrlist;
+		return new Disposables(smrlist, stream);
+	}
+
+	[Test]
+	public void WalkThrough_CheckMerkleTree() {
+		var serializer = new StringSerializer();
+		var recycledHash = Hashers.ZeroHash(CHF.SHA2_256);
+		using var disposables = CreateList(serializer, StringComparer.InvariantCultureIgnoreCase, out var rlist);
+
+		byte[] TreeHash(params string[] text) => MerkleTree.ComputeMerkleRoot(text.Select(x => x is not null ? Hashers.Hash(CHF.SHA2_256, serializer.SerializeLE(x)) : recycledHash), CHF.SHA2_256);
+
+		var mlist = (StreamMappedMerkleRecyclableList<string>)rlist;
+
+		// verify empty
+		Assert.That(rlist.Count, Is.EqualTo(0));
+		Assert.That(rlist.ListCount, Is.EqualTo(0));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(0));
+		Assert.That(mlist.MerkleTree.Root, Is.Null);
+
+		// Enumerate empty
+		CollectionAssert.AreEqual(rlist, Array.Empty<string>());
+
+		// add "A"
+		rlist.Add("A");
+		Assert.That(rlist.Count, Is.EqualTo(1));
+		Assert.That(rlist.ListCount, Is.EqualTo(1));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(0));
+		Assert.That(mlist.MerkleTree.Root, Is.EqualTo(TreeHash("A")));
+
+		// try insert
+		Assert.That(() => rlist.Insert(0, "text"), Throws.InstanceOf<NotSupportedException>());
+
+		// add "B" and "C"
+		rlist.AddRange(new[] { "B", "C" });
+		Assert.That(rlist.Count, Is.EqualTo(3));
+		Assert.That(rlist.ListCount, Is.EqualTo(3));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(0));
+		Assert.That(mlist.MerkleTree.Root, Is.EqualTo(TreeHash("A", "B", "C")));
+
+		// remove "B"
+		Assert.That(rlist.Remove("B"), Is.True);
+		Assert.That(rlist.Count, Is.EqualTo(2));
+		Assert.That(rlist.ListCount, Is.EqualTo(3));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(1));
+		Assert.That(mlist.MerkleTree.Root, Is.EqualTo(TreeHash("A", null, "C")));
+		
+		// Ensure B is not found
+		Assert.That(rlist.IndexOf("B"), Is.EqualTo(-1));
+
+		// try read recycled
+		Assert.That(() => rlist.Read(1), Throws.ArgumentException);
+		Assert.That(() => rlist.Update(1, "X"), Throws.ArgumentException);
+		Assert.That(() => rlist.RemoveAt(1), Throws.ArgumentException);
+
+		// Enumerate 
+		CollectionAssert.AreEqual(rlist, new[] { "A", "C" });
+		Assert.That(rlist.Count, Is.EqualTo(2));
+		Assert.That(rlist.ListCount, Is.EqualTo(3));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(1));
+
+		// add "B1" (verify used recycled index)
+		rlist.Add("B1");
+		Assert.That(rlist.Count, Is.EqualTo(3));
+		Assert.That(rlist.ListCount, Is.EqualTo(3));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(0));
+		Assert.That(rlist.IndexOf("B1"), Is.EqualTo(1));
+		Assert.That(rlist.Read(1), Is.EqualTo("B1"));
+		Assert.That(mlist.MerkleTree.Root, Is.EqualTo(TreeHash("A", "B1", "C")));
+
+		// Update B1 to B2
+		rlist.Update(1, "B2");
+		Assert.That(rlist.IndexOf("B2"), Is.EqualTo(1));
+		Assert.That(rlist.Count, Is.EqualTo(3));
+		Assert.That(rlist.ListCount, Is.EqualTo(3));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(0));
+		Assert.That(rlist.IndexOf("B2"), Is.EqualTo(1));
+		Assert.That(rlist.Read(1), Is.EqualTo("B2"));
+		Assert.That(mlist.MerkleTree.Root, Is.EqualTo(TreeHash("A", "B2", "C")));
+
+		// Enumeration check
+		CollectionAssert.AreEqual(rlist, new[] { "A", "B2", "C" });
+
+		// add another "A" (verify used new index)
+		rlist.Add("A");
+		Assert.That(rlist.ListCount, Is.EqualTo(4));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(0));
+		Assert.That(rlist.Count, Is.EqualTo(4));
+		Assert.That(mlist.MerkleTree.Root, Is.EqualTo(TreeHash("A", "B2", "C", "A")));
+
+		// verify index of first "A"
+		Assert.That(rlist.IndexOf("A"), Is.EqualTo(0));
+
+		// Remove first A
+		rlist.RemoveAt(0);
+		Assert.That(rlist.ListCount, Is.EqualTo(4));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(1));
+		Assert.That(rlist.Count, Is.EqualTo(3));
+		Assert.That(mlist.MerkleTree.Root, Is.EqualTo(TreeHash(null, "B2", "C", "A")));
+
+		// Verify index of second "A"
+		Assert.That(rlist.IndexOf("A"), Is.EqualTo(3));
+
+		// Remove second "A"
+		rlist.RemoveAt(3);
+		Assert.That(rlist.ListCount, Is.EqualTo(4));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(2));
+		Assert.That(rlist.Count, Is.EqualTo(2));
+		Assert.That(mlist.MerkleTree.Root, Is.EqualTo(TreeHash(null, "B2", "C", null)));
+
+		// Verify recycled indices
+		Assert.That(rlist.IsRecycled(0), Is.True);
+		Assert.That(rlist.IsRecycled(1), Is.False);
+		Assert.That(rlist.IsRecycled(2), Is.False);
+		Assert.That(rlist.IsRecycled(3), Is.True);
+
+		// Verify not "A" is found
+		Assert.That(rlist.IndexOf("A"), Is.EqualTo(-1));
+		Assert.That(rlist.Contains("A"), Is.False);
+
+
+		// Verify index of B2 and C are (1, 2) respectively and other indices are recycled
+		Assert.That(rlist.IndexOf("B2"), Is.EqualTo(1));
+		Assert.That(rlist.IndexOf("C"), Is.EqualTo(2));
+
+		// Enumerate "B2" and "C"
+		CollectionAssert.AreEqual(rlist, new[] { "B2", "C" });
+
+		// Clear
+		rlist.Clear();
+		Assert.That(rlist.ListCount, Is.EqualTo(0));
+		Assert.That(rlist.RecycledCount, Is.EqualTo(0));
+		Assert.That(rlist.Count, Is.EqualTo(0));
+		Assert.That(mlist.MerkleTree.Root, Is.Null);
+	}
+
+}

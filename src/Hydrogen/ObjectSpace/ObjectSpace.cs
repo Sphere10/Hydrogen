fbe8794de0e7b9cb0c8313ece3edd2a7d910a529
Herman Schoenfeld <herman@sphere10.com>
2023-09-19 08:41:56 +1000

    Update to .NET 8

--- src/Hydrogen/ObjectSpace/ObjectSpace.cs
+++ src/Hydrogen/ObjectSpace/ObjectSpace.cs
@@ -1,33 +1,33 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Linq;
-//using System.Linq.Expressions;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
 
-//namespace Hydrogen.ObjectSpace {
-//	public class ObjectSpace {
+namespace Hydrogen.ObjectSpace {
+	public class ObjectSpace {
 
-//		public ObjectSpaceDefinition Definition { get; }
+		public ObjectSpaceDefinition Definition { get; }
 
-//		public ObjectContainer[] Containers { get; }
+		public ObjectContainer[] Containers { get; }
 
 
-//		public IStreamMappedRecyclableList<TItem> GetContainer<TItem>() {
-//			throw new NotImplementedException();
-//		}
+		public IStreamMappedRecyclableList<TItem> GetContainer<TItem>() {
+			throw new NotImplementedException();
+		}
 
-//		public IStreamMappedDictionary<TKey, TValue> GetContainer<TKey, TValue>() {
-//			throw new NotImplementedException();
-//		}
+		public IStreamMappedDictionary<TKey, TValue> GetContainer<TKey, TValue>() {
+			throw new NotImplementedException();
+		}
 
-//		public IReadOnlyDictionary<TMember, IFuture<TItem>> GetUniqueLookup<TItem, TMember>(Expression<Func<TItem, TMember>> memberExpression) {
-//			throw new NotImplementedException();
-//		}
+		public IReadOnlyDictionary<TMember, IFuture<TItem>> GetUniqueLookup<TItem, TMember>(Expression<Func<TItem, TMember>> memberExpression) {
+			throw new NotImplementedException();
+		}
 
-//		public ILookup<TMember, IFuture<TItem>> GetLookup<TItem, TMember>(Expression<Func<TItem, TMember>> memberExpression) {
-//			throw new NotImplementedException();
-//		}
+		public ILookup<TMember, IFuture<TItem>> GetLookup<TItem, TMember>(Expression<Func<TItem, TMember>> memberExpression) {
+			throw new NotImplementedException();
+		}
 
-//	}
-//}
+	}
+}
---
+++ src/Hydrogen/ObjectSpace/Writer/IObjectWriter.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Hydrogen;
+
+//public class IObjectPersistor {
+
+//	internal void Write(TItem item, EndianBinaryWriter writer);
+
+//	internal TItem Read(long byteSize, EndianBinaryReader reader);
+
+//}
+

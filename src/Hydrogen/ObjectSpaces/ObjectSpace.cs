2e741023f443e557f2e19e1bd464f4de35e3e868
Herman Schoenfeld <herman@sphere10.com>
2024-02-06 14:52:23 +1000

    Improved ObjectSpace implementation, added unit test

--- src/Hydrogen/ObjectSpaces/ObjectSpace.cs
+++ src/Hydrogen/ObjectSpaces/ObjectSpace.cs
@@ -8,25 +8,27 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
+using static Hydrogen.ObjectSpaces.ObjectSpaceDefinition;
 
 namespace Hydrogen.ObjectSpaces;
 
-public class ObjectSpace : SyncLoadableBase, ITransactionalObject, IDisposable {
+public class ObjectSpace : SyncLoadableBase, ISynchronizedObject, ITransactionalObject, IDisposable {
 
 	public event EventHandlerEx<object> Committing { add => _fileStream.Committing += value; remove => _fileStream.Committing -= value; }
 	public event EventHandlerEx<object> Committed { add => _fileStream.Committed += value; remove => _fileStream.Committed -= value; }
 	public event EventHandlerEx<object> RollingBack { add => _fileStream.RollingBack += value; remove => _fileStream.RollingBack -= value; }
-	public event EventHandlerEx<object> RolledBack { add => _fileStream.RolledBack += value; remove => _fileStream.RolledBack -= value; } 
+	public event EventHandlerEx<object> RolledBack { add => _fileStream.RolledBack += value; remove => _fileStream.RolledBack -= value; }
 
+	private readonly SynchronizedObject _lock;
 	private readonly TransactionalStream _fileStream;
 	private readonly StreamContainer _streamContainer;
 	private readonly SerializerFactory _serializerFactory;
 	private readonly ComparerFactory _comparerFactory;
+	private readonly IDictionary<Type, object> _collections;
+	private readonly InstanceTracker _instanceTracker;
 	private bool _loaded;
 
 	public ObjectSpace(HydrogenFileDescriptor file, ObjectSpaceDefinition objectSpaceDefinition, SerializerFactory serializerFactory, ComparerFactory comparerFactory, FileAccessMode accessMode = FileAccessMode.Default) {
@@ -37,6 +39,7 @@ public class ObjectSpace : SyncLoadableBase, ITransactionalObject, IDisposable {
 		Definition = objectSpaceDefinition;
 		_serializerFactory = serializerFactory;
 		_comparerFactory = comparerFactory;
+		_lock = new SynchronizedObject();
 		_fileStream = new TransactionalStream(
 			file,
 			accessMode.WithoutAutoLoad()
@@ -50,6 +53,8 @@ public class ObjectSpace : SyncLoadableBase, ITransactionalObject, IDisposable {
 			false
 		);
 		_loaded = false;
+		_collections = new Dictionary<Type, object>();
+		_instanceTracker = new InstanceTracker();
 		if (accessMode.HasFlag(FileAccessMode.AutoLoad))
 			Load();
 	}
@@ -60,27 +65,88 @@ public class ObjectSpace : SyncLoadableBase, ITransactionalObject, IDisposable {
 
 	public ObjectSpaceDefinition Definition { get; }
 
-	public ObjectContainer[] Containers { get; private set; }
+	public ISynchronizedObject ParentSyncObject { get => _lock.ParentSyncObject; set => _lock.ParentSyncObject = value; }
 
-	public virtual IStreamMappedRecyclableList<TItem> OpenContainer<TItem>() {
-		CheckLoaded();
-		var container = Containers[0];
+	public ReaderWriterLockSlim ThreadLock => _lock.ThreadLock;
+
+	public IDisposable EnterReadScope() => _lock.EnterReadScope();
+
+	public IDisposable EnterWriteScope() => _lock.EnterWriteScope();
+
+	public IEnumerable<TItem> GetAll<TItem>() {
 		throw new NotImplementedException();
 	}
 
-	public IStreamMappedDictionary<TKey, TValue> GetContainer<TKey, TValue>() {
-		CheckLoaded();
-		throw new NotImplementedException();
+	public TItem Get<TItem>(long index) {
+		if (!TryGet<TItem>(index, out var item))
+			throw new InvalidOperationException($"No {typeof(TItem).ToStringCS()} at index {index} found");
+		return item;
 	}
 
-	public IReadOnlyDictionary<TMember, IFuture<TItem>> GetUniqueLookup<TItem, TMember>(Expression<Func<TItem, TMember>> memberExpression) {
-		CheckLoaded();
-		throw new NotImplementedException();
+	public bool TryGet<TItem>(long index, out TItem item) {
+		// First try return an already fetched instance
+		if (_instanceTracker.TryGet(index, out item))
+			return true;	
+		
+		// Get underlying stream mapped collection
+		var objectList = GetList<TItem>();
+		
+		// Range check
+		if (0 > index || index >= objectList.Count)
+			return false;
+
+		// Reap check
+		if (objectList.ObjectContainer.IsReaped(index))
+			return false;
+
+		// Deserialize from stream
+		item = objectList.ObjectContainer.LoadItem(index);
+
+		// Track item instance
+		_instanceTracker.Track(item, index);
+
+		return true;
 	}
 
-	public ILookup<TMember, IFuture<TItem>> GetLookup<TItem, TMember>(Expression<Func<TItem, TMember>> memberExpression) {
-		CheckLoaded();
-		throw new NotImplementedException();
+	public long Save<TItem>(TItem item) {
+		// Get underlying stream mapped collection
+		var objectList = GetList<TItem>();
+
+		// Get the item index (if applicable)
+		var existingItem = _instanceTracker.TryGetIndexOf(item, out var index);
+		
+		if (existingItem) {
+			// Update existing
+			objectList.Update(index, item);
+		} else {
+			// Add if new
+			objectList.Add(item, out index);
+			_instanceTracker.Track(item, index);
+		}
+
+		return index;
+	}
+
+	public void Delete<TItem>(TItem item) {
+		// Get tracked index of item instance
+		if (!_instanceTracker.TryGetIndexOf(item, out var index)) 
+			throw new InvalidOperationException($"Instance of {item.GetType().ToStringCS()} was not tracked");
+		
+		// Stop tracking instance
+		_instanceTracker.Untrack(item);
+
+		// Remove it from the list 
+		var objectList = GetList<TItem>();
+		objectList.Recycle(index);
+	}
+
+	private StreamMappedRecyclableList<TItem> GetList<TItem>() {
+		var itemType = typeof(TItem);
+
+		if (!_collections.TryGetValue(itemType, out var dimension))
+			throw new InvalidOperationException($"A container for type '{itemType.ToStringCS()}' was not registered");
+
+		return (StreamMappedRecyclableList<TItem>)dimension;
 	}
 
 	public void Commit() => _fileStream.Commit();
@@ -92,19 +158,18 @@ public class ObjectSpace : SyncLoadableBase, ITransactionalObject, IDisposable {
 	public Task RollbackAsync() => _fileStream.RollbackAsync();
 
 	public void Dispose() {
-		if (Containers is not null)
-			foreach(var container in Containers.Where(x => !x.RequiresLoad)) 
-				container.Dispose(); 
+		//foreach (var container in _collections.Values.Cast<ObjectContainer>().Where(x => !x.RequiresLoad))
+		//	container.Dispose();
+		foreach (var disposable in _collections.Values.Cast<IDisposable>())
+			disposable.Dispose();
+
 		_streamContainer.Dispose();
 		_fileStream.Dispose();
 	}
 
 	protected override void LoadInternal() {
 		Guard.Against(_loaded, "ObjectSpace already loaded.");
-		var validationResult = Validate(Definition);
-		if (validationResult.IsFailure) {
-			throw new InvalidOperationException(validationResult.ErrorMessages.ToParagraphCase());
-		}
+		Definition.Validate().ThrowOnFailure();
 
 		// Load up stream container
 		if (_streamContainer.RequiresLoad)
@@ -117,129 +182,115 @@ public class ObjectSpace : SyncLoadableBase, ITransactionalObject, IDisposable {
 			for (var i = 0; i < Definition.Containers.Length; i++) {
 				using var _ = _streamContainer.Add();
 			}
-		} 
+		}
 		containerStreams = _streamContainer.Header.StreamCount - _streamContainer.Header.ReservedStreams;
 		Guard.Ensure(containerStreams == Definition.Containers.Length, $"Unexpected stream count {Definition?.Containers.Length}, expected {containerStreams}");
 
 
 		// Here parse the internal structure and create the containers.
-		var containers = new List<ObjectContainer>();
-		foreach (var (containerDefinition, ix) in Definition.Containers.WithIndex()) {
-			// Get the stream within the object space which will comprise the object container
-			var containerStream = _streamContainer.Open(_streamContainer.Header.ReservedStreams + ix, false, true);
-
-			// Create a StreamContainer mapped to this stream, so it can contain items
-			var containerItemsStreamContainer = new StreamContainer(
-				containerStream,
-				SanitizeContainerClusterSize(containerDefinition.AverageObjectSizeBytes),
-				StreamContainerPolicy.FastAllocate,
-				containerDefinition.Indexes.Length,
-				Endianness.LittleEndian,
-				false
-			) {
-				OwnsStream = true
-			};
-
-			// construct the object container
-			var container = 
-				typeof(ObjectContainer<>)
-					.MakeGenericType(containerDefinition.ObjectType)
-					.ActivateWithCompatibleArgs(
-						containerItemsStreamContainer,
-						CreateItemSerializer(containerDefinition.ObjectType),
-						false
-					) as ObjectContainer;
-			container.OwnsStreamContainer = true;
-
-			// construct indexes
-			foreach (var (item, index) in containerDefinition.Indexes.WithIndex()) {
-				IObjectContainerAttachment metaDataObserver = item.Type switch {
-					ObjectSpaceDefinition.IndexType.UniqueKey => BuildUniqueKey(container, containerDefinition, item, index),
-					ObjectSpaceDefinition.IndexType.Index => BuildIndex(container, containerDefinition, item, index),
-					ObjectSpaceDefinition.IndexType.FreeIndexStore => new RecyclableIndexIndex(container, index),
-					ObjectSpaceDefinition.IndexType.MerkleTree => throw new NotImplementedException(),
-					_ => throw new ArgumentOutOfRangeException()
-				};
-				container.RegisterAttachment(metaDataObserver);
-			}
-
-			// load container
-			if (container.RequiresLoad)
-				container.Load();
-
-			containers.Add(container);
+		for (var ix = 0; ix < Definition.Containers.Length; ix++) {
+			var containerDefinition = Definition.Containers[ix];
+			var collection = BuildObjectList(containerDefinition, ix);
+			_collections.Add(containerDefinition.ObjectType, collection);
 		}
-		Containers = containers.ToArray();
 		_loaded = true;
 
-		IObjectContainerAttachment BuildUniqueKey(ObjectContainer container, ObjectSpaceDefinition.ContainerDefinition containerDefinition, ObjectSpaceDefinition.IndexDefinition indexDefinition, int streamIndex) {
-			var keyComparer = _comparerFactory.GetEqualityComparer(indexDefinition.KeyMember.PropertyType);
-			var keySerializer = _serializerFactory.GetSerializer(indexDefinition.KeyMember.PropertyType);
-			return 
-				keySerializer.IsConstantSize ? 
-					IndexFactory.CreateUniqueKeyIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, keyComparer) : 
-					IndexFactory.CreateUniqueKeyChecksumIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, null, null, keyComparer);
-		}
+	}
+	protected virtual object BuildObjectList(ContainerDefinition containerDefinition, int containerIndex) {
+		// Get the stream within the object space which will comprise the object container
+		var containerStream = _streamContainer.Open(_streamContainer.Header.ReservedStreams + containerIndex, false, true);
 
-		IObjectContainerAttachment BuildIndex(ObjectContainer container, ObjectSpaceDefinition.ContainerDefinition containerDefinition, ObjectSpaceDefinition.IndexDefinition indexDefinition, int streamIndex) {
-			var keyComparer = _comparerFactory.GetEqualityComparer(indexDefinition.KeyMember.PropertyType);
-			var keySerializer = _serializerFactory.GetSerializer(indexDefinition.KeyMember.PropertyType);
-			return 
-				keySerializer.IsConstantSize ?
-					IndexFactory.CreateKeyIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, keyComparer) :
-					IndexFactory.CreateKeyChecksumIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, null, null, keyComparer);
-				
-		}
+		// Create a StreamContainer mapped to this stream, so it can contain items
+		var containerItemsStreamContainer = new StreamContainer(
+			containerStream,
+			SanitizeContainerClusterSize(containerDefinition.AverageObjectSizeBytes),
+			StreamContainerPolicy.FastAllocate,
+			containerDefinition.Indexes.Length,
+			Endianness.LittleEndian,
+			false
+		) {
+			OwnsStream = true
+		};
 
+		// construct the object container
+		var container =
+			typeof(ObjectContainer<>)
+				.MakeGenericType(containerDefinition.ObjectType)
+				.ActivateWithCompatibleArgs(
+					containerItemsStreamContainer,
+					CreateItemSerializer(containerDefinition.ObjectType),
+					false
+				) as ObjectContainer;
+		container.OwnsStreamContainer = true;
+
+		// construct indexes
+		foreach (var (item, index) in containerDefinition.Indexes.WithIndex()) {
+			IObjectContainerAttachment metaDataObserver = item.Type switch {
+				IndexType.Identifier => BuildIdentifier(container, containerDefinition, item, index),
+				IndexType.UniqueKey => BuildUniqueKey(container, containerDefinition, item, index),
+				IndexType.Index => BuildIndex(container, containerDefinition, item, index),
+				IndexType.FreeIndexStore => new RecyclableIndexIndex(container, index),
+				IndexType.MerkleTree => throw new NotImplementedException(),
+				_ => throw new ArgumentOutOfRangeException()
+			};
+			container.RegisterAttachment(metaDataObserver);
+		}
+	
+		// Get a comparer
+		var comparer = _comparerFactory.GetEqualityComparer(containerDefinition.ObjectType);
+
+		// construct the the object collection
+		var list = typeof(StreamMappedRecyclableList<>)
+			.MakeGenericType(containerDefinition.ObjectType)
+			.ActivateWithCompatibleArgs(
+				container,
+				comparer,
+				false
+			);
+		Tools.Reflection.SetPropertyValue(list, nameof(StreamMappedRecyclableList<int>.OwnsContainer), true);
+
+		// load list if applicable
+		if (list is ILoadable { RequiresLoad: true } loadable)
+			loadable.Load();
+
+		
+
+		return list;
 	}
 
-	protected Result Validate(ObjectSpaceDefinition definition) {
-		var result = Result.Success;
-
-		// Verify all containers
-		if (definition.Containers != null) {
-			// At least 1 container
-			if (definition.Containers.Length == 0) {
-				result.AddError("At least 1 container must be defined.");
-			}
-
-			// Verify container
-			foreach (var (container, ix) in definition.Containers.WithIndex()) {
-				if (container.Indexes != null && container.Indexes.Length > 0) {
-					// Ensure has 1 free-index store
-					var freeIndexStores = container.Indexes.Count(x => x.Type == ObjectSpaceDefinition.IndexType.FreeIndexStore);
-					if (freeIndexStores != 1)
-						result.AddError($"Container {ix} ({container.ObjectType.ToStringCS()}) had {freeIndexStores} FreeIndexStore's defined, required {1}.");
-
-					// TODO: Ensure has 0 or 1 merkle-trees
-
-				} else {
-					result.AddError("Container requires index definition");
-				}
-			}
-
-
-			// All containers must have a unique type
-			definition
-				.Containers
-				.GroupBy(x => x.ObjectType)
-				.Where(x => x.Count() > 1)
-				.ForEach(x => result.AddError($"Container type '{x.Key}' is defined more than once."));
-
-			// Each container index correctly maps to an object property
-
-		} else {
-			result.AddError("No containers defined.");
-		}
-
-		return result;
+	protected virtual IObjectContainerAttachment BuildIdentifier(ObjectContainer container, ObjectSpaceDefinition.ContainerDefinition containerDefinition, ObjectSpaceDefinition.IndexDefinition indexDefinition, int streamIndex) {
+		var keyComparer = _comparerFactory.GetEqualityComparer(indexDefinition.KeyMember.PropertyType);
+		var keySerializer = _serializerFactory.GetSerializer(indexDefinition.KeyMember.PropertyType);
+		return
+			keySerializer.IsConstantSize ?
+				IndexFactory.CreateUniqueKeyIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, keyComparer) :
+				IndexFactory.CreateUniqueKeyChecksumIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, null, null, keyComparer);
 	}
 
-	protected IItemSerializer CreateItemSerializer(Type objectType) {
+	protected virtual IObjectContainerAttachment BuildUniqueKey(ObjectContainer container, ObjectSpaceDefinition.ContainerDefinition containerDefinition, ObjectSpaceDefinition.IndexDefinition indexDefinition, int streamIndex) {
+		var keyComparer = _comparerFactory.GetEqualityComparer(indexDefinition.KeyMember.PropertyType);
+		var keySerializer = _serializerFactory.GetSerializer(indexDefinition.KeyMember.PropertyType);
+		return
+			keySerializer.IsConstantSize ?
+				IndexFactory.CreateUniqueKeyIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, keyComparer) :
+				IndexFactory.CreateUniqueKeyChecksumIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, null, null, keyComparer);
+	}
+
+	protected virtual IObjectContainerAttachment BuildIndex(ObjectContainer container, ObjectSpaceDefinition.ContainerDefinition containerDefinition, ObjectSpaceDefinition.IndexDefinition indexDefinition, int streamIndex) {
+		var keyComparer = _comparerFactory.GetEqualityComparer(indexDefinition.KeyMember.PropertyType);
+		var keySerializer = _serializerFactory.GetSerializer(indexDefinition.KeyMember.PropertyType);
+		return
+			keySerializer.IsConstantSize ?
+				IndexFactory.CreateKeyIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, keyComparer) :
+				IndexFactory.CreateKeyChecksumIndexAttachment(container, streamIndex, indexDefinition.KeyMember, keySerializer, null, null, keyComparer);
+
+	}
+	
+	protected virtual IItemSerializer CreateItemSerializer(Type objectType) {
 		return _serializerFactory.GetSerializer(objectType);
 	}
 
-	protected int SanitizeContainerClusterSize(int clusterSizeB)
+	protected virtual int SanitizeContainerClusterSize(int clusterSizeB)
 		=> Tools.Values.ClipValue(clusterSizeB, 256, 8192);
 
 }
---
+++ tests/Hydrogen.Tests/ObjectSpaces/ObjectSpacesTest.cs
@@ -0,0 +1,130 @@
+﻿// Copyright (c) Sphere 10 Software. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT software license, see the accompanying file
+// LICENSE or visit http://www.opensource.org/licenses/mit-license.php.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Hydrogen.ObjectSpaces;
+using NUnit.Framework;
+
+namespace Hydrogen.Tests;
+
+[TestFixture, Timeout(60000)]
+public class ObjectSpacesTest {
+
+	[Test]
+	public void ConstructorDoesntThrow() {
+		var folder = Tools.FileSystem.GetTempEmptyDirectory(true);
+		var filePath = Path.Combine(folder, "app.db");
+		using var disposables = new Disposables(new ActionDisposable(() => Tools.FileSystem.DeleteDirectory(filePath)));
+		Assert.That( () => new ObjectSpace(BuildFileDefinition(filePath), BuildSpaceDefinition(), SerializerFactory.Default, ComparerFactory.Default), Throws.Nothing);
+	}
+
+	[Test]
+	public void WalkThrough() {
+		var folder = Tools.FileSystem.GetTempEmptyDirectory(true);
+		var filePath = Path.Combine(folder, "app.db");
+		using var disposables = new Disposables(new ActionDisposable(() => Tools.FileSystem.DeleteDirectory(filePath)));
+		using var appSpace = new ObjectSpace(BuildFileDefinition(filePath), BuildSpaceDefinition(), SerializerFactory.Default, ComparerFactory.Default);
+		
+		var secret = "MyPassword";
+
+		var dss = DSS.PQC_WAMSSharp;
+		var privateKey = Signers.CreatePrivateKey(dss, Hashers.Hash(CHF.SHA2_256, secret.ToAsciiByteArray()));
+		var publicKey = Signers.DerivePublicKey(dss, privateKey, 0);
+
+		var identity = new Identity {
+			DSS = dss,
+			Key = publicKey.RawBytes
+		};
+
+		var account = new Account {
+			Identity = identity,
+			Name = "Savings",
+			Quantity = 0
+		};
+
+		appSpace.Save(account);
+
+		appSpace.Commit();
+
+	}
+
+	private static HydrogenFileDescriptor BuildFileDefinition(string filePath) 
+		=> HydrogenFileDescriptor.From(
+			filePath, 
+			8192, 
+			Tools.Memory.ToBytes(50, MemoryMetric.Megabyte), 
+			512, 
+			StreamContainerPolicy.Default
+		);
+
+	private static ObjectSpaceDefinition BuildSpaceDefinition() {
+		var definition = new ObjectSpaceDefinition {
+			Containers = new ObjectSpaceDefinition.ContainerDefinition[] {
+				new() { 
+					ObjectType = typeof(Account),
+					Indexes = new ObjectSpaceDefinition.IndexDefinition[] {
+						new() {
+							Type = ObjectSpaceDefinition.IndexType.FreeIndexStore,
+							ReservedStreamIndex = 0,
+						},
+						new() {
+							Type = ObjectSpaceDefinition.IndexType.UniqueKey,
+							KeyMember = Tools.Mapping.GetMember<Account, string>(x => x.Name),
+							ReservedStreamIndex = 1
+						}
+					}
+				},
+				new() { 
+					ObjectType = typeof(Identity),
+					Indexes = new ObjectSpaceDefinition.IndexDefinition[] {
+						new() {
+							Type = ObjectSpaceDefinition.IndexType.FreeIndexStore,
+							ReservedStreamIndex = 0,
+						},
+						new() {
+							Type = ObjectSpaceDefinition.IndexType.UniqueKey,
+							KeyMember = Tools.Mapping.GetMember<Identity, byte[]>(x => x.Key),
+							ReservedStreamIndex = 0
+						}
+					}
+				},
+
+
+			}
+		};
+
+
+		return definition;
+	}
+
+
+	public class Account {
+		
+		[UniqueProperty]
+		public string Name { get; set; }
+		
+		public decimal Quantity { get; set; }
+
+		public Identity Identity { get; set; }
+
+	}
+
+	public class Identity { 
+
+		public DSS DSS { get; set; }
+
+		[UniqueProperty]
+		public byte[] Key { get; set; }
+	}
+
+}

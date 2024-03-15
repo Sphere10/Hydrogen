fb478914c6a0f73265bc80456128a45b4f0e1995
Herman Schoenfeld <herman@sphere10.com>
2024-03-15 16:32:43 +1000

    Added ClusteredStreamsAttachmentDecorator class

---
+++ src/Hydrogen/ClusteredStreams/Attachments/ClusteredStreamsAttachmentDecorator.cs
@@ -0,0 +1,31 @@
+﻿namespace Hydrogen;
+
+public class ClusteredStreamsAttachmentDecorator<TInner> : IClusteredStreamsAttachment 
+	where TInner : IClusteredStreamsAttachment {
+
+	protected readonly TInner Inner;
+
+	protected ClusteredStreamsAttachmentDecorator(TInner innerAttachment) {
+		Inner = innerAttachment;
+	}
+
+	public virtual ClusteredStreams Streams => Inner.Streams;
+
+	public virtual long ReservedStreamIndex => Inner.ReservedStreamIndex;
+
+	public virtual bool IsAttached => Inner.IsAttached;
+
+	public virtual void Attach() => Inner.Attach();
+
+	public virtual void Detach() => Inner.Detach();
+
+	public virtual void Flush() => Inner.Flush();
+}
+
+
+public class ClusteredStreamsAttachmentDecorator : ClusteredStreamsAttachmentDecorator<IClusteredStreamsAttachment> {
+
+	protected ClusteredStreamsAttachmentDecorator(IClusteredStreamsAttachment innerAttachment) 
+		: base(innerAttachment) {
+	}
+}

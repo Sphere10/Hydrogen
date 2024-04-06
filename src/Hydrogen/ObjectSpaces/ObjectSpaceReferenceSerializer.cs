cbc87919211ec9e6e66958afe2aabe2e5f3f9c8b
Herman Schoenfeld <herman@sphere10.com>
2024-04-07 09:26:27 +1000

    Fix build

--- src/Hydrogen/ObjectSpaces/ObjectSpaceReferenceSerializer.cs
+++ src/Hydrogen/ObjectSpaces/ObjectSpaceReferenceSerializer.cs
@@ -1,14 +1,14 @@
 ﻿
-namespace Hydrogen;
+//namespace Hydrogen;
 
-/// <summary>
-/// Serializer wrapper for handling reference types within an ObjectSpace. If a type is tracked within a dimension of the object-space,
-/// then this serializer will save it within that dimension rather than within the object itself. 
-/// </summary>
-public sealed class ObjectSpaceReferenceSerializer<TItem> : ReferenceSerializer<TItem> {
+///// <summary>
+///// Serializer wrapper for handling reference types within an ObjectSpace. If a type is tracked within a dimension of the object-space,
+///// then this serializer will save it within that dimension rather than within the object itself. 
+///// </summary>
+//public sealed class ObjectSpaceReferenceSerializer<TItem> : ReferenceSerializer<TItem> {
 
-	public ObjectSpaceReferenceSerializer(IItemSerializer<TItem> valueSerializer) : base(valueSerializer) {
-	}
-	public ObjectSpaceReferenceSerializer(IItemSerializer<TItem> valueSerializer, ReferenceSerializerMode mode) : base(valueSerializer, mode) {
-	}
-}
+//	public ObjectSpaceReferenceSerializer(IItemSerializer<TItem> valueSerializer) : base(valueSerializer) {
+//	}
+//	public ObjectSpaceReferenceSerializer(IItemSerializer<TItem> valueSerializer, ReferenceSerializerMode mode) : base(valueSerializer, mode) {
+//	}
+//}

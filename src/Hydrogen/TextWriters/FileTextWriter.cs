27ed50b7d577bfbdad9111f8fe8734297e90fd5e
Herman Schoenfeld <herman@sphere10.com>
2024-06-04 18:30:38 +1000

    Text Writers: added FileTextWriter

---
+++ src/Hydrogen/TextWriters/FileTextWriter.cs
@@ -0,0 +1,54 @@
+// Copyright (c) Sphere 10 Software. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT software license, see the accompanying file
+// LICENSE or visit http://www.opensource.org/licenses/mit-license.php.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hydrogen;
+
+/// <summary>
+/// TextWriter which writes to a file stream.
+/// </summary>
+/// <remarks></remarks>
+public class FileTextWriter : TextWriterBase {
+
+	/// <summary>
+	/// This is the default encoding used by StreamWriter, which File.AppendAllText uses internally.
+	/// </summary>
+	private static Encoding _swEncoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
+
+	private readonly TextWriter _textWriter;
+	
+	public FileTextWriter(string filePath, FileMode fileMode) 
+		: this(new FileStream(filePath, fileMode, FileAccess.Write), _swEncoding) {
+	}
+
+	public FileTextWriter(FileStream fileStream, Encoding encoding) {
+		_swEncoding = encoding;
+		FileStream = fileStream;
+		_textWriter = new StreamWriter(FileStream, encoding);
+	}
+
+	public FileStream FileStream { get; set; }
+
+	protected override void Dispose(bool disposing) {
+		_textWriter.Flush();
+		FileStream.Flush();
+		FileStream.Close();
+		FileStream.Dispose();
+		base.Dispose(disposing);
+	}
+
+	protected override void InternalWrite(string value) 
+		 => _textWriter.Write(value);
+
+	protected override Task InternalWriteAsync(string value) 
+		 => _textWriter.WriteAsync(value);
+
+}

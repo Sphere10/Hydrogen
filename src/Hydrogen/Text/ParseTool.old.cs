eace2bb9bae984d297a0ae9b45584933b115ef84
Herman Schoenfeld <herman@sphere10.com>
2024-06-09 10:54:21 +1000

    Change license and header to MIT NON-AI

--- src/Hydrogen/Text/ParseTool.old.cs
+++ src/Hydrogen/Text/ParseTool.old.cs
@@ -1,8 +1,8 @@
 //// Copyright (c) Sphere 10 Software. All rights reserved. (https://sphere10.com)
 //// Author: Herman Schoenfeld
 ////
-//// Distributed under the MIT software license, see the accompanying file
-//// LICENSE or visit http://www.opensource.org/licenses/mit-license.php.
+//// Distributed under the MIT NON-AI software license, see the accompanying file
+//// LICENSE or visit https://sphere10.com/legal/NON-AI-MIT.
 ////
 //// This notice must not be removed when duplicating this file or its contents, in whole or in part.
 

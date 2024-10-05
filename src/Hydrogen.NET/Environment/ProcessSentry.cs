845f0c8064125b3f9178757e7bd680c0c720c832
Herman Schoenfeld <herman@sphere10.com>
2024-10-06 00:52:37 +1000

    Prorcess Sentry Fix

--- src/Hydrogen.NET/Environment/ProcessSentry.cs
+++ src/Hydrogen.NET/Environment/ProcessSentry.cs
@@ -15,17 +15,19 @@ using System.Threading.Tasks;
 namespace Hydrogen;
 
 public class ProcessSentry {
-	private readonly SynchronizedObject _lock;
+	//private readonly SynchronizedObject _lock;
 	private Process _runningProcess;
 
 	public ProcessSentry(string executableFileName) {
 		ExecutableFileName = executableFileName;
-		_lock = new SynchronizedObject();
+		//_lock = new SynchronizedObject();
 	}
+
+	public string WorkingDirectory { get; init; } = Tools.Runtime.GetExecutablePath();
 
 	public string ExecutableFileName { get; }
 
-	public TextWriter Output { get; init; } = new NoOpTextWriter();
+	public TextWriter OutputWriter { get; init; } = new NoOpTextWriter();
 
 	protected Process RunningProcess => _runningProcess;
 
@@ -34,21 +36,21 @@ public class ProcessSentry {
 	}
 
 	public virtual async Task RunWithErrorCodeCheckAsync(string arguments = null, int expectedErrorCode = 0, CancellationToken cancellationToken = default) {
-		using (_lock.EnterWriteScope()) {
+		//using (_lock.EnterWriteScope()) {
 			var stringBuilderTextWriter = new StringBuilderTextWriter();
 
 			var errorCode = await RunAsync(arguments, cancellationToken);
 			if (errorCode != expectedErrorCode)
 				throw new ProcessSentryException(ExecutableFileName, errorCode, stringBuilderTextWriter.Builder.ToString());
-		}
+		//}
 	}
 
 	public virtual async Task<int> RunAsync(string arguments = null, CancellationToken cancellationToken = default) {
-		using (_lock.EnterWriteScope()) {
+		//using (_lock.EnterWriteScope()) {
 			_runningProcess = new Process();
 			var _ = new ActionDisposable(() => _runningProcess = null);
-			return await RunProcess(_runningProcess, ExecutableFileName, arguments, Output, cancellationToken);
-		}
+			return await RunProcessInternal(_runningProcess, ExecutableFileName, WorkingDirectory, arguments, OutputWriter, cancellationToken);
+		//}
 	}
 
 	public static async Task<bool> CanRunAsync(string executableFileName, CancellationToken cancellationToken = default) {
@@ -61,11 +63,12 @@ public class ProcessSentry {
 		return true;
 	}
 
-	public static Task<int> RunAsync(string fileName, string arguments = null, TextWriter output = null, CancellationToken cancellationToken = default)
-		=> RunProcess(new Process(), fileName, arguments, output, cancellationToken);
+	public static Task<int> RunAsync(string fileName, string workingDir, string arguments = null, TextWriter output = null, CancellationToken cancellationToken = default)
+		=> RunProcessInternal(new Process(), fileName, workingDir, arguments, output, cancellationToken);
 
-	private static async Task<int> RunProcess(Process process, string fileName, string arguments = null, TextWriter output = null, CancellationToken cancellationToken = default) {
+	private static async Task<int> RunProcessInternal(Process process, string fileName, string workingDir, string arguments = null, TextWriter output = null, CancellationToken cancellationToken = default) {
 		Guard.ArgumentNotNull(process, nameof(process));
+		process.StartInfo.WorkingDirectory = workingDir;
 		process.StartInfo.FileName = fileName;
 		process.StartInfo.Arguments = arguments;
 		process.StartInfo.ErrorDialog = false;
@@ -80,7 +83,12 @@ public class ProcessSentry {
 			var syncObj = new SynchronizedObject();
 			process.OutputDataReceived += async (_, data) => {
 				using (syncObj.EnterWriteScope())
-					await output.WriteLineAsync(data.Data);
+					output.WriteLine(data.Data);
+			};
+
+			process.ErrorDataReceived += async (_, data) => {
+				using (syncObj.EnterWriteScope())
+					output.WriteLine(data.Data);
 			};
 		}
 
